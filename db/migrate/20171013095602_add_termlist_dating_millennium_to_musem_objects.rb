ab9f56e266a48ed07ce3c0b61288670f059705ab
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2017-10-13 12:02:33 +0200

    Adds association (belongs_to) between museum_object and dating millenium

--- db/migrate/20171013095602_add_termlist_dating_millennium_to_musem_objects.rb
+++ db/migrate/20171013095602_add_termlist_dating_millennium_to_musem_objects.rb
@@ -1,5 +1,5 @@
 class AddTermlistDatingMillenniumToMusemObjects < ActiveRecord::Migration[5.0]
   def change
-    add_reference :musem_objects, :termlist_dating_millennium, foreign_key: true
+    add_reference :museum_objects, :termlist_dating_millennium, foreign_key: true
   end
 end
