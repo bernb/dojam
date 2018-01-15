6f26c75089c62d375559deb90b158c57248f1978
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-01-15 14:56:41 +0100

    Seed correct storage entries

--- db/seeds/seed_storages.rb
+++ db/seeds/seed_storages.rb
@@ -2,18 +2,38 @@ museum = Museum.where(name: "JAM").first
 
 storages = []
 
-('A'..'C').each do |n|
-  storage = Storage.create name: "Hall " + n.to_s
-  storages.push storage
-end
+storageA = Storage.create name: "hall A"
+storageB = Storage.create name: "hall B"
+storageC = Storage.create name: "hall C"
+storages << storageA
+storages << storageB
+storages << storageC
 
-storages.each do |storage|
-  (1..30).each do |n|
+
+storageA.each do |storage|
+  (1..28).each do |n|
   letter = storage.name[-1] # counts backwards the string thus gets the last char
   location = StorageLocation.create name: "showcase " + letter + n.to_s
   storage.storage_locations << location
   end
 end
+
+storageB.each do |storage|
+  (1..5).each do |n|
+  letter = storage.name[-1] # counts backwards the string thus gets the last char
+  location = StorageLocation.create name: "showcase " + letter + n.to_s
+  storage.storage_locations << location
+  end
+end
+
+storageA.each do |storage|
+  (1..11).each do |n|
+  letter = storage.name[-1] # counts backwards the string thus gets the last char
+  location = StorageLocation.create name: "showcase " + letter + n.to_s
+  storage.storage_locations << location
+  end
+end
+
 
 museum.storages << storages
 
