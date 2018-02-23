427b49f3a40193e1f656ec82784ffd220e935526
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-02-23 15:35:06 +0100

    Check the actual status at reload. Also correct line break. It's terribly done, sorry

--- app/assets/javascripts/build_steps.js
+++ app/assets/javascripts/build_steps.js
@@ -16,7 +16,34 @@ ready = function() {
 });
   */
   var dating_period_checkbox = $('#optional_termlist_dating_period_checkbox');
-  var dating_period_selection = dating_period_checkbox.next().next();
+  var dating_period_selection = dating_period_checkbox.next().next().next();
+  
+  var dating_millennium_checkbox = $('#optional_termlist_dating_millennium_checkbox');
+  var dating_millennium_selection = dating_millennium_checkbox.next().next().next();
+  
+  var dating_centuries_checkbox = $('#optional_termlist_dating_centuries_checkbox');
+  var dating_centuries_selection = dating_centuries_checkbox.next().next().next();
+  
+  // Check initial state
+  if(dating_period_checkbox.get(0).checked) {
+    dating_period_selection.show();
+  } else {
+    dating_period_selection.hide();
+  }
+  
+  if(dating_millennium_checkbox.get(0).checked) {
+    dating_millennium_selection.show();
+  } else {
+    dating_millennium_selection.hide();
+  }
+  
+  if(dating_centuries_checkbox.get(0).checked) {
+    dating_centuries_selection.show();
+  } else {
+    dating_centuries_selection.hide();
+  }
+  
+  
   dating_period_checkbox.change(function() {
     if(this.checked) {
       dating_period_selection.show();
@@ -25,8 +52,7 @@ ready = function() {
     }
   });
   
-  var dating_millennium_checkbox = $('#optional_termlist_dating_millennium_checkbox');
-  var dating_millennium_selection = dating_millennium_checkbox.next().next();
+  
   dating_millennium_checkbox.change(function() {
     if(this.checked) {
       dating_millennium_selection.show();
@@ -35,8 +61,6 @@ ready = function() {
     }
   });
   
-  var dating_centuries_checkbox = $('#optional_termlist_dating_centuries_checkbox');
-  var dating_centuries_selection = dating_centuries_checkbox.next().next();
   dating_centuries_checkbox.change(function() {
     if(this.checked) {
       dating_centuries_selection.show();
