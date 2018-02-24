5f11686ff123daee8beedd510efb06e8dfbb8d95
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-02-24 11:41:41 +0100

    Now uses site-specific js by checking body class

--- app/assets/javascripts/build_steps.js
+++ app/assets/javascripts/build_steps.js
@@ -1,75 +1,11 @@
+// shows how to apply a controller-action combination
 var ready;
+var site_name = ".builds .show"; 
+
 ready = function() {
-  // ToDo: More robust selector instead of double next()
-  // ToDo: Generalize by using a general selector as shown below and then
-  //       iterate all matching to get the wanted behaviour for all
-  /*
-  var checkbox_selector = "[id^='optional_termlist'][id$='checkbox']";
-  var optional_termlist_selector = $(checkbox_selector).next().next();
-  $(optional_termlist_selector).hide();
-  $(checkbox_selector).change(function() {
-    if(this.checked) {
-      $(optional_termlist_selector).show();
-    } else {
-      $(optional_termlist_selector).hide();
-    }
-});
-  */
-  var dating_period_checkbox = $('#optional_termlist_dating_period_checkbox');
-  var dating_period_selection = dating_period_checkbox.next().next().next();
-  
-  var dating_millennium_checkbox = $('#optional_termlist_dating_millennium_checkbox');
-  var dating_millennium_selection = dating_millennium_checkbox.next().next().next();
-  
-  var dating_centuries_checkbox = $('#optional_termlist_dating_centuries_checkbox');
-  var dating_centuries_selection = dating_centuries_checkbox.next().next().next();
-  
-  // Check initial state
-  if(dating_period_checkbox.get(0).checked) {
-    dating_period_selection.show();
-  } else {
-    dating_period_selection.hide();
+  if($(document.body).is(site_name)){
+  site_specific_js();
   }
-  
-  if(dating_millennium_checkbox.get(0).checked) {
-    dating_millennium_selection.show();
-  } else {
-    dating_millennium_selection.hide();
-  }
-  
-  if(dating_centuries_checkbox.get(0).checked) {
-    dating_centuries_selection.show();
-  } else {
-    dating_centuries_selection.hide();
-  }
-  
-  
-  dating_period_checkbox.change(function() {
-    if(this.checked) {
-      dating_period_selection.show();
-    } else {
-      dating_period_selection.hide();
-    }
-  });
-  
-  
-  dating_millennium_checkbox.change(function() {
-    if(this.checked) {
-      dating_millennium_selection.show();
-    } else {
-      dating_millennium_selection.hide();
-    }
-  });
-  
-  dating_centuries_checkbox.change(function() {
-    if(this.checked) {
-      dating_centuries_selection.show();
-    } else {
-      dating_centuries_selection.hide();
-    }
-  });
-  
-  
 };
 
 
---
+++ app/assets/javascripts/step_dating.js
@@ -0,0 +1,85 @@
+var ready;
+var site_name = "step_dating";
+
+ready = function() {
+  if($(document.body).hasClass(site_name)){
+  site_specific_js();
+  }
+};
+
+function site_specific_js() {
+    // ToDo: More robust selector instead of double next()
+  // ToDo: Generalize by using a general selector as shown below and then
+  //       iterate all matching to get the wanted behaviour for all
+  /*
+  var checkbox_selector = "[id^='optional_termlist'][id$='checkbox']";
+  var optional_termlist_selector = $(checkbox_selector).next().next();
+  $(optional_termlist_selector).hide();
+  $(checkbox_selector).change(function() {
+    if(this.checked) {
+      $(optional_termlist_selector).show();
+    } else {
+      $(optional_termlist_selector).hide();
+    }
+});
+  */
+  var dating_period_checkbox = $('#optional_termlist_dating_period_checkbox');
+  var dating_period_selection = dating_period_checkbox.next().next().next();
+  
+  var dating_millennium_checkbox = $('#optional_termlist_dating_millennium_checkbox');
+  var dating_millennium_selection = dating_millennium_checkbox.next().next().next();
+  
+  var dating_centuries_checkbox = $('#optional_termlist_dating_centuries_checkbox');
+  var dating_centuries_selection = dating_centuries_checkbox.next().next().next();
+  
+  // Check initial state
+  if(dating_period_checkbox.get(0).checked) {
+    dating_period_selection.show();
+  } else {
+    dating_period_selection.hide();
+  }
+  
+  if(dating_millennium_checkbox.get(0).checked) {
+    dating_millennium_selection.show();
+  } else {
+    dating_millennium_selection.hide();
+  }
+  
+  if(dating_centuries_checkbox.get(0).checked) {
+    dating_centuries_selection.show();
+  } else {
+    dating_centuries_selection.hide();
+  }
+  
+  
+  dating_period_checkbox.change(function() {
+    if(this.checked) {
+      dating_period_selection.show();
+    } else {
+      dating_period_selection.hide();
+    }
+  });
+  
+  
+  dating_millennium_checkbox.change(function() {
+    if(this.checked) {
+      dating_millennium_selection.show();
+    } else {
+      dating_millennium_selection.hide();
+    }
+  });
+  
+  dating_centuries_checkbox.change(function() {
+    if(this.checked) {
+      dating_centuries_selection.show();
+    } else {
+      dating_centuries_selection.hide();
+    }
+  });
+  
+  
+} // end of site specific js function
+
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
