1977f875cd3bd4ec982fe59df5d6d7e7d45722ac
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-05-09 11:26:45 +0200

    corrects visibility on button click

--- app/assets/javascripts/museum_objects.js
+++ app/assets/javascripts/museum_objects.js
@@ -1,10 +1,6 @@
 var ready;
 ready = function() {
-  $('.museum_object_page_button').click(function(event) {
-    var id = event.target.id;
-    $('.museum_object_page').not('.'+id).addClass('hidden');
-    $('.'+id).removeClass('hidden');
-  }); 
+  
   
   $("tr[data-link]").click(function() { // Used in search result table / index view
     window.location = $(this).data("link")
