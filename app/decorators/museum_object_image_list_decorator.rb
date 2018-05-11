4c0c54b05dbf0823db2418ac8470f805453964ff
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-05-11 20:20:29 +0200

    Removed all use of active storage for image as it does not support to convert tiff or organize a proper png variant in any way

--- app/decorators/museum_object_image_list_decorator.rb
+++ app/decorators/museum_object_image_list_decorator.rb
@@ -2,9 +2,21 @@ class MuseumObjectImageListDecorator < Draper::Decorator
   include Draper::LazyHelpers
   delegate_all
   
-  def remove_from_list image_id
-    self.list.find(image_id).purge
-    #redirect_back(fallback_location: root_path)
+  def main_image_link
+    html = ""
+    if self.main.nil?
+      html = "not defined"
+    else
+      html = 
+        "<div class=\"col-4\">" + 
+          self.link_to(main.blob.filename, main, class: "card-img-top", target:"_blank") + 
+        "</div> 
+         <div class=\"col-4\">" +
+          self.link_to("remove", delete_image_path(main.id, main.id), method: :delete, confirm: "Delete image?", class: "text-warning") +
+        "</div>"
+    end
+    return helpers.sanitize html
   end
+
 
 end
