53dd2c538f68350d5589ff57443b2fe11d47ec7d
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-05-13 01:28:04 +0200

    Finetuning of rails_admin navigations / labels

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -20,8 +20,19 @@ RailsAdmin.config do |config|
       "TermlistKindOfObjectSpecifiedsInscriptionLetter",
       "TermlistKindOfObjectSpecifiedsPreservationMaterial",
       "TermlistKindOfObjectSpecifiedsPreservationObject",
-      "TermlistKindOfObjectSpecifiedsProductionTechnique"
+      "TermlistKindOfObjectSpecifiedsProductionTechnique",
+      "ActiveStorage::Attachment",
+      "ActiveStorage::Blob",
+      "MuseumObjectImageList"
     ]
+  # eager load throws error :(
+  # note this must be before model excluding  
+  #Rails.application.eager_load!
+  #ActiveRecord::Base.descendants.each do |imodel|
+  #  config.model "#{imodel.name}" do
+  #    label label.split(' ',2)[1]
+  #  end
+  #end
 
   config.excluded_models |= hiding_models
   
@@ -37,13 +48,132 @@ RailsAdmin.config do |config|
     
   # not working unfortunately
   config.model TermlistColor do
-    create do
-      include_all_fields
-      field :museum_objects do
-        form_value = MuseumObject.first
-      end
-    end
+    label label.split(' ',2)[1]
+    #create do
+    #  include_all_fields
+    #  field :museum_objects do
+    #    form_value = MuseumObject.first
+    #  end
+    #end
   end
+  
+  config.model Museum do
+    navigation_label "Museum"
+  end
+  
+  config.model MuseumObject do
+    navigation_label "Museum"
+    label "museum object"
+  end
+  
+  config.model Storage do
+    parent Museum
+    navigation_label "Museum"
+  end
+  
+  config.model StorageLocation do
+    parent StorageLocation
+    navigation_label "Museum"
+  end
+  
+  config.model TermlistAcquisitionDeliveredBy do
+    label label.split(' ',2)[1]
+  end
+  
+  config.model TermlistAcquisitionKind do
+    label label.split(' ',2)[1]
+  end
+  
+  config.model TermlistAuthenticity do
+    label label.split(' ',2)[1]
+  end
+  
+  config.model TermlistConservation do
+    label label.split(' ',2)[1]
+  end
+  
+  config.model TermlistDatingCentury do
+    navigation_label "Dating"
+    label label.split(' ',2)[1]
+  end
+  
+  config.model TermlistDatingMillennium do
+    navigation_label "Dating"
+    label label.split(' ',2)[1]
+  end
+  
+  config.model TermlistDatingPeriod do
+    navigation_label "Dating"
+    label label.split(' ',2)[1]
+  end
+  
+  config.model TermlistDecoration do
+    navigation_label "Decoration"
+    label label.split(' ',2)[1]
+  end
+  
+  config.model TermlistDecorationTechnique do
+    navigation_label "Decoration"
+    label label.split(' ',2)[1]
+  end
+  
+  config.model TermlistDecorationColor do
+    navigation_label "Decoration"
+    label "color"
+  end
+  
+  config.model TermlistExcavationSiteCategory do
+    label label.split(' ',2)[1]
+  end
+  
+  config.model TermlistExcavationSiteKind do
+    label label.split(' ',2)[1]
+  end
+  
+  config.model TermlistInscriptionLanguage do
+    label label.split(' ',2)[1]
+  end
+  
+  config.model TermlistInscriptionLetter do
+    label label.split(' ',2)[1]
+  end
+  
+  config.model TermlistKindOfObject do
+    label label.split(' ',2)[1]
+  end
+  
+  config.model TermlistKindOfObjectSpecified do
+    parent TermlistKindOfObject
+    label label.split(' ',2)[1]
+  end
+  
+  config.model TermlistMaterialSpecified do
+    label label.split(' ',2)[1]
+    parent TermlistMaterial
+  end
+  
+  config.model TermlistMaterial do
+    label label.split(' ',2)[1]
+  end
+  
+  config.model TermlistPreservationMaterial do
+    navigation_label "Preservation"
+    label label.split(' ',2)[1]
+  end
+  
+  config.model TermlistPreservationObject do
+    navigation_label "Preservation"
+    label label.split(' ',2)[1]
+  end
+  
+  config.model TermlistProductionTechnique do
+    label label.split(' ',2)[1]
+  end
+  
+  
+  # rails_admin will look for inv_number on models
+  # i.e. museum_objects for naming
+  config.label_methods << :inv_number
 
 
   ### Popular gems integration
