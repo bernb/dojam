da9dbfa48874a73404081b40544a59ffb38d6427
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-05-14 16:39:44 +0200

    Recognize nil values for timespans and return empty string

--- app/decorators/museum_object_decorator.rb
+++ app/decorators/museum_object_decorator.rb
@@ -5,6 +5,10 @@ class MuseumObjectDecorator < Draper::Decorator
   def dating_timespan
     timespan_begin_suffix = is_dating_timespan_begin_BC? ? " BC" : " AD"
     timespan_end_suffix = is_dating_timespan_end_BC? ? " BC" : " AD"
-    return dating_timespan_begin&.strftime("%Y") + timespan_begin_suffix + " - " + dating_timespan_end&.strftime("%Y") + timespan_end_suffix
+    if dating_timespan_begin.present? && dating_timespan_end.present?
+      return dating_timespan_begin&.strftime("%Y") + timespan_begin_suffix + " - " + dating_timespan_end&.strftime("%Y") + timespan_end_suffix
+    else
+      return ""
+    end
   end
 end
