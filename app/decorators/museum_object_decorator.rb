6a183e35327b61babb34802e6d75fe1c99bcefe4
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-05-14 16:03:21 +0200

    Decorate museum object to show proper timespan format

--- app/decorators/museum_object_decorator.rb
+++ app/decorators/museum_object_decorator.rb
@@ -1,5 +1,10 @@
 class MuseumObjectDecorator < Draper::Decorator
   include Draper::LazyHelpers
   delegate_all
-
+  
+  def dating_timespan
+    timespan_begin_suffix = is_dating_timespan_begin_BC? ? " BC" : " AD"
+    timespan_end_suffix = is_dating_timespan_end_BC? ? " BC" : " AD"
+    return dating_timespan_begin&.strftime("%Y") + timespan_begin_suffix + " - " + dating_timespan_end&.strftime("%Y") + timespan_end_suffix
+  end
 end
