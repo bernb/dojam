8166d516647b89fbb06c76070223effd324338f4
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-05-26 23:22:40 +0200

    Overwrite shovel << operator to respect database unique constrain of foregin keys

--- app/models/termlist_kind_of_object_specified.rb
+++ app/models/termlist_kind_of_object_specified.rb
@@ -3,7 +3,11 @@ class TermlistKindOfObjectSpecified < ApplicationRecord
   belongs_to :termlist_kind_of_object, required: false
   has_many :museum_objects
 	has_many :material_specifieds_koo_specs
-	has_many :termlist_material_specifieds, through: :material_specifieds_koo_specs
+	has_many :termlist_material_specifieds, through: :material_specifieds_koo_specs do
+		def << (value)
+			super value unless self.include? value
+		end
+	end
 	has_many :colors_ms_koo_specs, through: :material_specifieds_koo_specs
 	has_many :termlist_colors, through: :colors_ms_koo_specs
 end
--- app/models/termlist_material_specified.rb
+++ app/models/termlist_material_specified.rb
@@ -3,7 +3,11 @@ class TermlistMaterialSpecified < ApplicationRecord
   has_many :join_museum_object_material_specifieds
   has_many :museum_objects, through: :join_museum_object_material_specifieds
 	has_many :material_specifieds_koo_specs
-  has_many :termlist_kind_of_object_specifieds, through: :material_specifieds_koo_specs
+  has_many :termlist_kind_of_object_specifieds, through: :material_specifieds_koo_specs do
+		def << (value)
+			super value unless self.include? value
+		end
+	end
 	has_many :termlist_kind_of_objects, through: :termlist_kind_of_object_specifieds
 
 end
