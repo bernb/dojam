83ef6d9c558c70781237553cae271697b98c0640
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-05-26 12:05:13 +0200

    Deactivate tracer for full stack error

--- config/initializers/trace_system_stack_error.rb
+++ config/initializers/trace_system_stack_error.rb
@@ -1,10 +1,13 @@
-$enable_tracing = false
-$trace_out = open('trace.txt', 'w')
+# Can be used to debug full stack error
+# Which does not give a stacktrace by itself
 
-set_trace_func proc { |event, file, line, id, binding, classname|
-  if $enable_tracing && event == 'call'
-    $trace_out.puts "#{file}:#{line} #{classname}##{id}"
-  end
-}
-
-$enable_tracing = true
+# $enable_tracing = false
+# $trace_out = open('trace.txt', 'w')
+# 
+# set_trace_func proc { |event, file, line, id, binding, classname|
+#   if $enable_tracing && event == 'call'
+#     $trace_out.puts "#{file}:#{line} #{classname}##{id}"
+#   end
+# }
+# 
+# $enable_tracing = true
