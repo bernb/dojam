ef34dc46b7ed0daa0ad2d8f6e9a181046b95b5fd
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-05-29 15:11:00 +0200

    Fix Bug: Wrong label and value method

--- app/inputs/termlist_select_input.rb
+++ app/inputs/termlist_select_input.rb
@@ -1,8 +1,8 @@
 class TermlistSelectInput < SimpleForm::Inputs::CollectionSelectInput
 	def input(wrapper_options)
 		merged_inputs = merge_wrapper_options(input_html_options, wrapper_options)
-		label_method = :to_s
-    value_method = :to_s
+		label_method = :name
+    value_method = :id
 
     @builder.collection_select(
       attribute_name, collection, value_method, label_method,
