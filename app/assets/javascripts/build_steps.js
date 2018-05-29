83f1727346178f999a12c889bb30266e8a5bdba9
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-05-29 14:50:40 +0200

    Use client-side validation now (only kind_of_object step for now): Disallow empty val but allow to save nil on server side

--- app/assets/javascripts/build_steps.js
+++ app/assets/javascripts/build_steps.js
@@ -1,13 +1,27 @@
 // shows how to apply a controller-action combination
 var ready;
-var site_name = ".builds .show"; 
+var site_name = ".builds"; 
+
+function termlist_undetermined_handler() {
+	var length = $(".termlist_select_input").children('option').length;
+	var undetermined_option = $(".termlist_select_input option[value=nil]");
+	if (length > 1 && undetermined_option.length == 0) { // i.e. not only blank entry present and undetermined option missing
+	  $(".termlist_select_input").append(new Option("undetermined","nil"));
+	}
+}
+function step_show_build_steps_js() {
+	termlist_undetermined_handler();
+}
+
 
 ready = function() {
   if($(document.body).is(site_name)){
-  //step_show_js();
+  step_show_build_steps_js();
   }
 };
 
+$( document ).ajaxComplete(function() {
+	termlist_undetermined_handler();
+	});
 
-$(document).ready(ready);
-$(document).on('page:load', ready);
+$(document).on('turbolinks:load', ready);
