a38ecc75bd0743dffdc961ac89755a32813f28a4
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-05-30 09:33:06 +0200

    Show undetermined for empty termlist selects

--- app/assets/javascripts/build_steps.js
+++ app/assets/javascripts/build_steps.js
@@ -5,7 +5,8 @@ var site_name = ".builds";
 function termlist_undetermined_handler() {
 	var length = $(".termlist_select_input").children('option').length;
 	var undetermined_option = $(".termlist_select_input option[value=nil]");
-	if (length > 1 && undetermined_option.length == 0) { // i.e. not only blank entry present and undetermined option missing
+	// Show undetermined for empty selects for now
+	if (/*length > 1 &&*/ undetermined_option.length == 0) { // i.e. not only blank entry present and undetermined option missing
 	  $(".termlist_select_input").append(new Option("undetermined","nil"));
 	}
 }
