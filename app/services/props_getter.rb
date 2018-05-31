645f01157016a8a9134b0dc97375d1a181805c70
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-05-31 17:17:32 +0200

    Correct query for second case

--- app/services/props_getter.rb
+++ app/services/props_getter.rb
@@ -4,9 +4,18 @@ class PropsGetter
 		return nil unless property_name.class == String && property_name.starts_with?("Termlist") && /^[a-zA-Z_]+$/.match?(property_name)
 		class_name = Object.const_get(property_name)
 		if termlist_material_specified.present? && termlist_kind_of_object_specified.present?
-			class_name.joins(:material_specifieds_koo_specs)
-				.where(termlist_material_specified: termlist_material_specified)
-				.where(termlist_kind_of_object_specified: termlist_kind_of_object_specified)
+			koos_id = termlist_kind_of_object_specified.id
+			m_spec_id = termlist_material_specified.id
+			class_name.joins(material_specifieds_koo_specs: 
+											 [:termlist_kind_of_object_specified,  
+												{termlist_material_specified: :termlist_material}])
+				.where(material_specifieds_koo_specs: 
+								{termlist_kind_of_object_specifieds: 
+										{id: koos_id}})
+				.where(material_specifieds_koo_specs: 
+					{termlist_material_specifieds: 
+						{id: m_spec_id}})
+				.distinct
 		elsif termlist_material_specified.present? && termlist_kind_of_object.present?
 			# Just to make the query a bit more clear
 			m_spec_id = termlist_material_specified.id
