d5b376cae69ca7d9506537342ee8570ac604c799
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-05-31 08:17:25 +0200

    First version of props getter service for museum object. Seems to wrong, committing for historical reasons.

--- app/services/props_getter.rb
+++ app/services/props_getter.rb
@@ -1,6 +1,7 @@
 class PropsGetter
 
 	def self.call(termlist_material_specified: nil, termlist_kind_of_object_specified: nil, termlist_kind_of_object: nil, property_name: nil)
+		return nil unless property_name.class == String && property_name.starts_with?("Termlist") && /^[a-zA-Z_]+$/.match?(property_name)
 		class_name = Object.const_get(property_name)
 		if termlist_material_specified.present? && termlist_kind_of_object_specified.present?
 			class_name.joins(:material_specifieds_koo_specs)
