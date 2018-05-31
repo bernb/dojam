c309ebef4c2b0598636c57868fe49c86f7fd1b3b
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-05-31 19:42:01 +0200

    Remove now obsolet models

--- test/models/termlist_kind_of_object_specifieds_decoration_technique_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TermlistKindOfObjectSpecifiedsDecorationTechniqueTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/termlist_kind_of_object_specifieds_inscription_language_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TermlistKindOfObjectSpecifiedsInscriptionLanguageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
