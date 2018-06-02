f17e78f7a871a0e82d6a8e900c80de5de6151c33
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-06-02 21:56:38 +0200

    Only use koos if given, as koos also defines the implicit main material if several are given

--- app/services/props_getter.rb
+++ app/services/props_getter.rb
@@ -3,21 +3,15 @@ class PropsGetter
 	def self.call(termlist_material_specified: nil, termlist_kind_of_object_specified: nil, termlist_kind_of_object: nil, property_name: nil)
 		return nil unless property_name.class == String && property_name.starts_with?("Termlist") && /^[a-zA-Z_]+$/.match?(property_name)
 		class_name = Object.const_get(property_name)
-		if termlist_material_specified.present? && termlist_kind_of_object_specified.present?
+		if termlist_kind_of_object_specified.present?
 			koos_id = termlist_kind_of_object_specified.id
-			m_spec_id = termlist_material_specified.id
-			Rails.logger.debug "**** Props getter: ms/koos selection ****"
+			Rails.logger.debug "**** Props getter: koos selection ****"
 			Rails.logger.debug "class_name: " + class_name.to_s
 			Rails.logger.debug "koos_id: " + koos_id.to_s
-			Rails.logger.debug "m_spec_id: " + m_spec_id.to_s
-			class_name.joins(material_specifieds_koo_specs: 
-											 [:termlist_kind_of_object_specified, :termlist_material_specified])
+			class_name.joins(material_specifieds_koo_specs: :termlist_kind_of_object_specified)
 				.where(material_specifieds_koo_specs: 
 								{termlist_kind_of_object_specifieds: 
 										{id: koos_id}})
-				.where(material_specifieds_koo_specs: 
-					{termlist_material_specifieds: 
-						{id: m_spec_id}})
 				.distinct
 		elsif termlist_material_specified.present? && termlist_kind_of_object.present?
 			Rails.logger.debug "**** Props getter: ms/koo selection ****"
