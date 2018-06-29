b3a7e4bde2aed0dc87e59c871c102f389d53ae65
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-06-29 16:22:45 +0200

    First step to write values into hidden fields

--- app/assets/javascripts/administrate/termlist_select.js
+++ app/assets/javascripts/administrate/termlist_select.js
@@ -4,6 +4,13 @@ $(function () {
 			$(this).data("selected", true);
 			$(this).removeClass("btn-light");
 			$(this).addClass("btn-success");
+			if($(this).data("level") == "material") {
+				values = $("#selected_materials").val();
+				if(values.indexOf("1") == -1) {
+					values.push("1");
+					$("#selected_materials").val(values);
+				}
+			}
 		} else {
 			$(this).data("selected", false);
 			$(this).removeClass("btn-success");
