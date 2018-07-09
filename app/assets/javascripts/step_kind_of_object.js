43e0ed50a99ddb9dfe7137af5c71cb06fcb89d14
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-07-09 15:31:11 +0200

    Load kind of object list initial on turbolinks:load event

--- app/assets/javascripts/step_kind_of_object.js
+++ app/assets/javascripts/step_kind_of_object.js
@@ -1,5 +1,13 @@
 var ready;
 
+function get_kind_of_object_list() {
+	$.ajax({
+		url: "/builds/kind_of_objects_for_spec_material",
+		type: "GET",
+		data: {selected_material_specified_id: $('input[name=museum_object\\[termlist_material_specified_id\\]]:checked').val()}
+	});
+}
+
 ready = function() {
   var site_name = ".step_kind_of_object"; 
   if($(document.body).is(site_name)){
@@ -8,14 +16,8 @@ ready = function() {
 };
 
 function step_kind_of_object_js() {
-  $('input[name=museum_object\\[termlist_material_specified_id\\]]').change(function() {
-    $.ajax({
-      url: "/builds/kind_of_objects_for_spec_material",
-      type: "GET",
-      data: {selected_material_specified_id: $('input[name=museum_object\\[termlist_material_specified_id\\]]:checked').val()}
-    });
-  });
+	get_kind_of_object_list();
+  $('input[name=museum_object\\[termlist_material_specified_id\\]]').change(get_kind_of_object_list());
 }
 
-$(document).ready(ready);
-$(document).on('page:load', ready);
+$(document).on('turbolinks:load', ready);
