f388ade8671ef2b8574f5c333f3dbda102916a6d
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-07-10 13:48:09 +0200

    default_scope order by name asc

--- app/models/termlist_preservation_material.rb
+++ app/models/termlist_preservation_material.rb
@@ -1,4 +1,5 @@
 class TermlistPreservationMaterial < ApplicationRecord
+	default_scope{order(name: :asc)}
   has_many :museum_objects
 	has_many :preservation_materials_ms_koo_specs
 	has_many :material_specifieds_koo_specs, through: :preservation_materials_ms_koo_specs
--- app/models/termlist_preservation_object.rb
+++ app/models/termlist_preservation_object.rb
@@ -1,4 +1,5 @@
 class TermlistPreservationObject < ApplicationRecord
+	default_scope{order(name: :asc)}
   has_many :museum_objects
 	has_many :preservation_objects_ms_koo_specs
 	has_many :material_specifieds_koo_specs, through: :preservation_objects_ms_koo_specs
