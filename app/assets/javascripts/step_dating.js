86c6f62cdb0c46ad415911fcbb71ec9e80abcd1a
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-08-21 14:11:01 +0200

    Frontend logic for dating

--- app/assets/javascripts/step_dating.js
+++ app/assets/javascripts/step_dating.js
@@ -1,91 +1,83 @@
+// shows how to apply a controller-action combination
 var ready;
 
 ready = function() {
-  var site_name = "step_dating";
-  if($(document.body).hasClass(site_name)){
-  step_dating_js();
+  var site_name = ".step_dating"; 
+  if($(document.body).is(site_name)){
+    step_dating_js();
   }
 };
 
+function set_dating_date_properties(disable) {
+		$('#museum_object_dating_year').prop('disabled', disable);
+		$('#museum_object_dating_month').prop('disabled', disable);
+		$('#museum_object_dating_day').prop('disabled', disable);
+	if(disable) {
+		$('#museum_object_dating_year').val("");
+		$('#museum_object_dating_month').val("");
+		$('#museum_object_dating_day').val("");
+	}
+}
+
+function dating_period_checkbox_logic() {
+	period_checkbox = $('#museum_object_is_dating_period_unknown');
+	period_checkbox_state = period_checkbox.is(':checked');
+	$('#museum_object_dating_period_id').prop('disabled', period_checkbox_state);
+	period_checkbox.change(function() {
+		period_dropdown = $('#museum_object_dating_period_id');
+		period_dropdown.val("");
+		period_dropdown.prop('disabled', $(this).is(':checked'));
+	});
+}
+
+function dating_millennium_checkbox_logic() {
+	millennium_checkbox = $('#museum_object_is_dating_millennium_unknown');
+	millennium_checkbox_state = millennium_checkbox.is(':checked');
+	$('#museum_object_dating_millennium_begin_id').prop('disabled', millennium_checkbox_state);
+	$('#museum_object_dating_millennium_end_id').prop('disabled', millennium_checkbox_state);
+	millennium_checkbox.change(function() {
+		millennium_dropdown_begin = $('#museum_object_dating_millennium_begin_id');
+		millennium_dropdown_begin.val("");
+		millennium_dropdown_begin.prop('disabled', $(this).is(':checked'));
+
+		millennium_dropdown_end = $('#museum_object_dating_millennium_end_id');
+		millennium_dropdown_end.val("");
+		millennium_dropdown_end.prop('disabled', $(this).is(':checked'));
+	});
+}
+
+function dating_timespan_checkbox_logic() {
+	timespan_checkbox = $('#museum_object_is_dating_timespan_unknown');
+	timespan_checkbox_state = timespan_checkbox.is(':checked');
+
+	$('#museum_object_dating_timespan_begin').prop('disabled', timespan_checkbox_state);
+	$('#museum_object_dating_timespan_end').prop('disabled', timespan_checkbox_state);
+	$('#museum_object_is_dating_timespan_begin_BC').prop('disabled', timespan_checkbox_state);
+	$('#museum_object_is_dating_timespan_end_BC').prop('disabled', timespan_checkbox_state);
+
+	timespan_checkbox.change(function() {
+		timespan_dropdown_begin = $('#museum_object_dating_timespan_begin');
+		timespan_dropdown_begin.val("");
+		timespan_dropdown_begin.prop('disabled', $(this).is(':checked'));
+
+		timespan_dropdown_end = $('#museum_object_dating_timespan_end');
+		timespan_dropdown_end.val("");
+		timespan_dropdown_end.prop('disabled', $(this).is(':checked'));
+
+		timespan_end_BC = $('#museum_object_is_dating_timespan_end_BC');
+		timespan_end_BC.val("");
+		timespan_end_BC.prop('disabled', $(this).is(':checked'));
+
+		timespan_begin_BC = $('#museum_object_is_dating_timespan_begin_BC');
+		timespan_begin_BC.val("");
+		timespan_begin_BC.prop('disabled', $(this).is(':checked'));
+	});
+}
+
 function step_dating_js() {
-    // ToDo: More robust selector instead of double next()
-  // ToDo: Generalize by using a general selector as shown below and then
-  //       iterate all matching to get the wanted behaviour for all
-  /*
-  var checkbox_selector = "[id^='optional_termlist'][id$='checkbox']";
-  var optional_termlist_selector = $(checkbox_selector).next().next();
-  $(optional_termlist_selector).hide();
-  $(checkbox_selector).change(function() {
-    if(this.checked) {
-      $(optional_termlist_selector).show();
-    } else {
-      $(optional_termlist_selector).hide();
-    }
-});
-  */
-  var dating_period_checkbox = $('#optional_termlist_dating_period_checkbox');
-  var dating_period_selection = dating_period_checkbox.next().next().next();
-  
-  var dating_millennium_checkbox = $('#optional_termlist_dating_millennium_checkbox');
-  var dating_millennium_selection = dating_millennium_checkbox.next().next().next();
-  
-  var dating_centuries_checkbox = $('#optional_termlist_dating_centuries_checkbox');
-  var dating_centuries_selection = dating_centuries_checkbox.next().next().next();
-  
-  // Check initial state
-  if(dating_period_checkbox.get(0).checked) {
-    dating_period_selection.show();
-  } else {
-    dating_period_selection.hide();
-    dating_period_selection.children().val("");
-  }
-  
-  if(dating_millennium_checkbox.get(0).checked) {
-    dating_millennium_selection.show();
-  } else {
-    dating_millennium_selection.hide();
-    dating_millennium_selection.children().val("");
-  }
-  
-  if(dating_centuries_checkbox.get(0).checked) {
-    dating_centuries_selection.show();
-  } else {
-    dating_centuries_selection.hide();
-    dating_centuries_selection.children().val("");
-  }
-  
-  
-  dating_period_checkbox.change(function() {
-    if(this.checked) {
-      dating_period_selection.show();
-    } else {
-      dating_period_selection.children().val("");
-      dating_period_selection.hide();
-    }
-  });
-  
-  
-  dating_millennium_checkbox.change(function() {
-    if(this.checked) {
-      dating_millennium_selection.show();
-    } else {
-      dating_millennium_selection.hide();
-      dating_millennium_selection.children().val("");
-    }
-  });
-  
-  dating_centuries_checkbox.change(function() {
-    if(this.checked) {
-      dating_centuries_selection.show();
-    } else {
-      dating_centuries_selection.hide();
-      dating_centuries_selection.children().val("");
-    }
-  });
-  
-  
-} // end of site specific js function
+	dating_period_checkbox_logic();
+	dating_millennium_checkbox_logic();
+	dating_timespan_checkbox_logic();
+}
 
-
-$(document).ready(ready);
-$(document).on('page:load', ready);
+$(document).on('turbolinks:load', ready);
