accf44b2f014633abcf8f647541bc67a7c3af6a6
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-09-06 15:59:21 +0200

    Recognize kind of object specifieds if existent

--- app/helpers/exporter_helper.rb
+++ app/helpers/exporter_helper.rb
@@ -9,6 +9,15 @@ module ExporterHelper
 		h[:material_specifieds] = material_specifieds.map(&:name)
 		kind_of_objects = material_specifieds.map(&:kind_of_objects).flatten.uniq
 		h[:kind_of_objects] = kind_of_objects.map(&:name)
+		kind_of_objects.each do |koo|
+			koos = koo.kind_of_object_specifieds(material_specified: material_specifieds.first).reject{|k| k.name == "undetermined"}
+			if koos.present?
+				koos_hash = Hash.new
+				koos_hash[koo.name] = koos.map(&:name)
+				h[:kind_of_objects].delete koo.name
+				h[:kind_of_objects] << koos_hash
+			end
+		end
 
 
 
