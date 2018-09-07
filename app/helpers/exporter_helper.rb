ca3b5a1f3b6ee3c23bc6bd8bdebe12748e60708c
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-09-07 10:21:44 +0300

    Complete first version of exporter, which just iterate over all materials and ignore material_specified customizations

--- app/helpers/exporter_helper.rb
+++ app/helpers/exporter_helper.rb
@@ -1,7 +1,41 @@
 module ExporterHelper
 	require 'awesome_print'
 
-	def self.call material
+	def self.call
+		relative_path = "exports/"
+		termlists = []
+		Material.all.each do |material|
+		termlists << export_termlists(material)
+		end
+		general_termlists = export_general_termlists
+
+		termlists.each do |termlist|
+			File.open(relative_path + termlist["material"].gsub('/', '_') + ".txt", "w") {|file| file.write(termlist.to_yaml)}
+		end
+			File.open(relative_path + "general_termlists" + ".txt", "w") {|file| file.write(general_termlists.to_yaml)}
+	end
+
+
+	def export_general_termlists
+		general_terms = Hash.new
+		general_terms["storages"] = Hash.new
+		Storage.all.map(&:name).each{|s| general_terms["storages"][s] = []}
+		StorageLocation.all.map{|s| [s.name, s.storage.name]}.each do |pair|
+			general_terms["storages"][pair.second] << pair.first
+		end
+
+		termlist_names = Termlist.all.map(&:type).uniq
+		termlist_names.each do |termlist_name|
+			termlist = termlist_name.constantize
+			if termlist.is_independent_of_paths
+				general_terms[termlist_name] = Termlist.where(type: termlist_name).uniq
+			end
+		end
+
+		return general_terms
+	end
+
+	def export_termlists material
 		h = Hash.new
 		general_terms = Hash.new
 		h["material"] = material.name
@@ -19,6 +53,9 @@ module ExporterHelper
 				h["kind_of_objects"] << koos_hash
 			end
 		end
+		
+		# We produce one wrong entry with the sql queries
+		h.delete("KindOfObject")
 
 		# Build termlists now
 		m_id = material.id
@@ -30,22 +67,10 @@ module ExporterHelper
 			if !termlist.is_independent_of_paths
 				terms = termlist.joins(:paths).where("paths.path LIKE ?", "/#{m_id}/#{ms_id}/#{koo_id}%").where("name != ?", "undetermined").uniq
 				h[termlist_name] = terms.map(&:name) unless terms.empty?
-			else
-				general_terms[termlist_name] = termlist.all.map(&:name)
 			end
 		end
 
-		general_terms["storages"] = Hash.new
-		Storage.all.map(&:name).each{|s| general_terms["storages"][s] = []}
-		StorageLocation.all.map{|s| [s.name, s.storage.name]}.each do |pair|
-			general_terms["storages"][pair.second] << pair.first
-		end
-
-		# We produce one wrong entry with the sql queries
-		h.delete("KindOfObject")
-
-		ap general_terms
-		ap h
+		return h
 	end
 
 end
