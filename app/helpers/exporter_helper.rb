6ac8d100830940e980872a27462084679b78b941
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-09-07 09:43:20 +0300

    Completes cli export

--- app/helpers/exporter_helper.rb
+++ app/helpers/exporter_helper.rb
@@ -4,19 +4,19 @@ module ExporterHelper
 	def self.call material
 		h = Hash.new
 		general_terms = Hash.new
-		h[:material] = material.name
+		h["material"] = material.name
 		material_specified_ids = material.paths.first.direct_children.map(&:ids).map(&:second)
 		material_specifieds = MaterialSpecified.find(material_specified_ids)
-		h[:material_specifieds] = material_specifieds.map(&:name)
+		h["material_specifieds"] = material_specifieds.map(&:name)
 		kind_of_objects = material_specifieds.map(&:kind_of_objects).flatten.uniq
-		h[:kind_of_objects] = kind_of_objects.map(&:name)
+		h["kind_of_objects"] = kind_of_objects.map(&:name)
 		kind_of_objects.each do |koo|
 			koos = koo.kind_of_object_specifieds(material_specified: material_specifieds.first).reject{|k| k.name == "undetermined"}
 			if koos.present?
 				koos_hash = Hash.new
 				koos_hash[koo.name] = koos.map(&:name)
-				h[:kind_of_objects].delete koo.name
-				h[:kind_of_objects] << koos_hash
+				h["kind_of_objects"].delete koo.name
+				h["kind_of_objects"] << koos_hash
 			end
 		end
 
@@ -33,6 +33,12 @@ module ExporterHelper
 			else
 				general_terms[termlist_name] = termlist.all.map(&:name)
 			end
+		end
+
+		general_terms["storages"] = Hash.new
+		Storage.all.map(&:name).each{|s| general_terms["storages"][s] = []}
+		StorageLocation.all.map{|s| [s.name, s.storage.name]}.each do |pair|
+			general_terms["storages"][pair.second] << pair.first
 		end
 
 		# We produce one wrong entry with the sql queries
