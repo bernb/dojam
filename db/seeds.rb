ac007305276b59aebcb0180f34d01b84416b8034
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-09-12 11:27:06 +0300

    Complete refactor of general termlists

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,22 @@ $museum_storage_data.keys.each do |museum_name|
 	museum.save!
 end
 
+# *** Import general termlists
+$general_terms_data.keys.each do |termlist_name|
+	classname = termlist_name.to_s.singularize.camelize.constantize
+	$general_terms_data[termlist_name].each do |name|
+		classname.find_or_create_by name: name
+	end
+end
+
+$site_kinds.each do |category_name, kinds_array|
+  category = ExcavationSiteCategory.find_or_create_by name: category_name
+  kinds_array.each do |site_kind_name|
+    site_kind = ExcavationSiteKind.find_or_create_by name: site_kind_name
+    category.excavation_site_kinds << site_kind
+  end
+end
+
 Rails.logger.info "*** Starting termlist import ***"
 global_variables.select{|var| var.to_s.ends_with? "_material_data"}
 								.reject{|var| var.to_s.include? "test"}
