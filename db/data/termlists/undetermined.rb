b051eed77034d273695f8ca7e7a02f4e852c8630
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-09-17 14:08:09 +0300

    Introduce 'undetermined' material

---
+++ db/data/termlists/undetermined.rb
@@ -0,0 +1,32 @@
+$undetermined_material_data = {
+
+	material_name: "undetermined",
+
+	material_specifieds: [
+		"undetermined"
+	],
+	kind_of_objects: [
+		"undetermined"
+	],
+	production_techniques: [
+		"undetermined"
+	],
+	decorations: [
+		"undetermined"
+	],
+	colors: [
+		"undetermined"
+	], 
+	decoration_colors: [
+		"undetermined"
+	], 
+	decoration_techniques: [
+		"undetermined"
+	],
+	preservation_materials: [
+		"undetermined"
+	],
+	preservation_objects: [
+		"undetermined"
+	]
+}
