c2ecead639df894a6f0f39c0043f9f2cc2cf17ed
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-09-20 15:50:00 +0300

    Kind of sorted out the decoration style inconsistency

--- app/models/museum_object.rb
+++ app/models/museum_object.rb
@@ -18,7 +18,7 @@ class MuseumObject < ApplicationRecord
   belongs_to :production_technique, required: false
   belongs_to :decoration_technique, required: false
   belongs_to :decoration_color, required: false
-  belongs_to :decoration_style, required: false
+  belongs_to :decoration, required: false, foreign_key: 'decoration_style_id'
   belongs_to :preservation_material, required: false
   belongs_to :preservation_object, required: false
   belongs_to :inscription_letter, required: false
@@ -260,9 +260,11 @@ class MuseumObject < ApplicationRecord
 										  :acquisition_kind,
 											:acquisition_delivered_by,
 											:excavation_site_kind,
+											:excavation_site_category,
 											:production_technique,
 											:decoration_technique,
 											:decoration_color,
+											:decoration,
 											:inscription_letter,
 											:inscription_language,
 											:preservation_material,
