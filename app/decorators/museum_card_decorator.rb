9e9629d57f9b549a45efabcb0a5fd16e520c6f7b
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-09-21 14:39:53 +0300

    Remove redundant use of museum card decorator

--- app/decorators/museum_card_decorator.rb
+++ app/decorators/museum_card_decorator.rb
@@ -2,47 +2,5 @@ class MuseumCardDecorator < Draper::Decorator
   decorates :museum_object
   delegate_all
 
-  def top_image
-    html = ""
-    if self&.images&.main&.attachment.nil?
-      html = "<div class=\"card-img-dummy\"></div>"
-    else
-      html = self.h.image_tag self.images.main, class: "card-img-top"
-    end
-    
-    return helpers.sanitize html
-  end
-  
-  def list_materials
-    list = ""
-    self&.materials.each do |material|
-      list << material.name << "<p>"
-    end
-    
-    return helpers.sanitize list
-  end
-
-  def list_material_specifieds
-    list = ""
-    self&.material_specifieds.each do |ms|
-      list << ms.name << "<p>"
-    end
-    
-    return helpers.sanitize list
-  end
-  
-  def links variant
-    links = ""
-    if variant.blank? || !variant.class == Hash
-      return  helpers.sanitize links
-    else
-      if variant.key?(:edit)
-        edit_link = h.link_to "edit", self.h.museum_object_build_path(id, :step_confirm)
-        links << edit_link
-      end
-    end
-    return  helpers.sanitize links
-  end
-  
   
 end
--- app/decorators/museum_object_decorator.rb
+++ app/decorators/museum_object_decorator.rb
@@ -39,7 +39,6 @@ class MuseumObjectDecorator < Draper::Decorator
 		else
 			self.max_width
 		end
-		j
 	end
 
 	def height_decorated
@@ -148,4 +147,48 @@ class MuseumObjectDecorator < Draper::Decorator
 	def needs_conservation_yesno?
 		self.needs_conservation ? "Yes" : "No"
 	end
+
+
+  def top_image
+    html = ""
+    if self&.images&.main&.attachment.nil?
+      html = "<div class=\"card-img-dummy\"></div>"
+    else
+      html = self.h.image_tag self.images.main, class: "card-img-top"
+    end
+    
+    return helpers.sanitize html
+  end
+  
+  def list_materials
+    list = ""
+    self&.materials.each do |material|
+      list << material.name << "<p>"
+    end
+    
+    return helpers.sanitize list
+  end
+
+  def list_material_specifieds
+    list = ""
+    self&.material_specifieds.each do |ms|
+      list << ms.name << "<p>"
+    end
+    
+    return helpers.sanitize list
+  end
+  
+  def links variant
+    links = ""
+    if variant.blank? || !variant.class == Hash
+      return  helpers.sanitize links
+    else
+      if variant.key?(:edit)
+        edit_link = h.link_to "edit", self.h.museum_object_build_path(id, :step_confirm)
+        links << edit_link
+      end
+    end
+    return  helpers.sanitize links
+  end
+  
 end
