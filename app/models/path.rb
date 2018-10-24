267a0df510575d8eabdb5b17ce7fdb33478bc581
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-10-24 15:37:56 +0300

    Add method to get path that only consists of undetermined entries

--- app/models/path.rb
+++ app/models/path.rb
@@ -4,6 +4,15 @@ class Path < ApplicationRecord
 	scope :last_id, ->(id) {where "path like ?", "%/#{id}"}
 	scope :depth, ->(depth) {where "path similar to ?", "/\\d{1,}" * depth}
 
+	def self.undetermined_path
+		m_id = Material.find_by(name: "undetermined").id.to_s
+		ms_id = MaterialSpecified.find_by(name: "undetermined").id.to_s
+		koo_id = KindOfObject.find_by(name: "undetermined").id.to_s
+		koos_id = KindOfObjectSpecified.find_by(name: "undetermined").id.to_s
+
+		return Path.find_by path: "/#{m_id}/#{ms_id}/#{koo_id}/#{koos_id}"
+	end
+
 	def root?
 		self.parent.nil?
 	end
