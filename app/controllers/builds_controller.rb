df44c8fef2edc7e98df90ecbf626c2d8f0cafc21
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2018-10-24 15:43:46 +0300

    Do not set main_path to nil anymore, we use Path.undetermined_path for this now

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -46,7 +46,7 @@ class BuildsController < ApplicationController
 				end
 			end
 			if !found
-				@museum_object.main_path = nil
+				@museum_object.main_path = Path.undetermined_path
 			end
 		end
 
