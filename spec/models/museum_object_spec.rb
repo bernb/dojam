4604a25fe5f31b3ce1c4e1dd1f6756e7718ef4cf
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2019-01-19 15:01:18 +0100

    No Goind through tests without errors

--- spec/models/museum_object_spec.rb
+++ spec/models/museum_object_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe MuseumObject, type: :model do
 	it "should allow to set single seconday path as array" do
-		museum_object = create(:valid_museum_object)
-		Path.sample(25).each do |path|
+		museum_object = create(:museum_object)
+		Path.all.sample(25).each do |path|
 			museum_object.secondary_path = path
+			expect(museum_object.secondary_paths.count).to eq(1)
+			expect(museum_object.secondary_path_ids.first).to eq(path.id)
 		end
 	end
 end
