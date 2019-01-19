09a4e65c266eb3da526d3279a75dabc642dcc03c
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2019-01-19 15:29:40 +0100

    Add specs for setting of secondary paths

--- spec/models/museum_object_spec.rb
+++ spec/models/museum_object_spec.rb
@@ -4,9 +4,36 @@ RSpec.describe MuseumObject, type: :model do
 	it "should allow to set single seconday path as array" do
 		museum_object = create(:museum_object)
 		Path.all.sample(25).each do |path|
-			museum_object.secondary_path = path
+			museum_object.secondary_paths = [path]
 			expect(museum_object.secondary_paths.count).to eq(1)
 			expect(museum_object.secondary_path_ids.first).to eq(path.id)
 		end
 	end
+
+	it "should ignore if tried to add a parent to existing secondary paths" do
+		museum_object = create(:museum_object)
+		Path.depth(2).sample(25).each do |child|
+			parent = child.parent
+			museum_object.secondary_paths = child
+			museum_object.secondary_paths << parent
+			expect(museum_object.secondary_paths.count).to eq(1)
+			expect(museum_object.secondary_path_ids.first).to eq(child.id)
+		end
+	end
+
+	it "should ignore if tried to add a parent of main path to secondary paths" do
+		museum_object = create(:museum_object)
+		Path.depth(2).sample(25).each do |child|
+			parent = child.parent
+			museum_object.main_path = child
+			museum_object.secondary_paths << parent
+			expect(museum_object.secondary_paths.count).to eq(0)
+		end
+	end
+
+	it "should move old main path information to secondary paths if new main path set"
+	it "should use parent paths if tried to set secondary paths of depth > 2"
+	it "should replace entries of a collection of secondary paths with more specific already existing paths"
+	it "should ignore if tried to add existing main path to secondary paths"
+	it "should only keep most specific secondary paths if new collection is added"
 end
