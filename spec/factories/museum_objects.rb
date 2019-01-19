93b1648012e61fabcda7452d44f42e533cb71399
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2019-01-19 14:36:37 +0100

    First working test with rspec and factory bot

--- spec/factories/museum_objects.rb
+++ spec/factories/museum_objects.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :museum_object do
-    
+  factory :valid_museum_object do
+		inv_number "T.1234"
   end
 end
--- spec/models/museum_object_spec.rb
+++ spec/models/museum_object_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe MuseumObject, type: :model do
-	it "should not throw an error" do
-		expect(1+200).to eq(201)
+	it "should allow to set single seconday path as array" do
+		museum_object = create(:valid_museum_object)
+		Path.sample(25).each do |path|
+			museum_object.secondary_path = path
+		end
 	end
 end
