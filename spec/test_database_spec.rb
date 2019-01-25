862816618156577115e6a54272a14fe055cd57f7
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2019-01-25 17:37:49 +0100

    Introduce simple tests to check if test database has any entries

---
+++ spec/test_database_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "Database for Testing" do
+	it "should have paths of depth one to four" do
+		expect(Path.depth(1).count).to be > 10
+		expect(Path.depth(2).count).to be > 10
+		expect(Path.depth(3).count).to be > 10
+		expect(Path.depth(4).count).to be > 10
+	end
+
+	it "should contain multiple termlists" do
+		expect(Termlist.all.count).to be > 10
+	end
+end
