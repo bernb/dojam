2f9167bb72f26e13435281e7db66af09a49bf953
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2019-01-30 17:44:41 +0100

    Add more tests for children relatec methods

--- spec/models/path_spec.rb
+++ spec/models/path_spec.rb
@@ -20,9 +20,21 @@ RSpec.describe Path, type: :model do
 		expect(path.child_of?(parent)).to be(true)
 	end
 
+	it "should return true if child_of?(path) is called on an indirect parent" do
+		path = Path.depth(3).sample
+		parent = path.parent.parent
+		expect(path.child_of?(parent)).to be(true)
+	end
+
 	it "should return true if included_or_child_of?(paths) is called on a path that's a parent of a path's element" do
 		parents = Path.depth(3).sample(5)
 		child = parents.sample.direct_children.sample
+		expect(child.included_or_child_of?(parents)).to be(true)
+	end
+
+	it "should return true if included_or_child_of?(paths) is called on a path that's an indirect parent of a path's element" do
+		parents = Path.depth(2).sample(5)
+		child = parents.sample.direct_children.sample.direct_children.sample
 		expect(child.included_or_child_of?(parents)).to be(true)
 	end
 
