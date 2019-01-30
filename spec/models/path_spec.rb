9316a28fdaf3dc252d037def030ed1c1061f3d59
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2019-01-30 17:40:06 +0100

    Add tests for child_of? method

--- spec/models/path_spec.rb
+++ spec/models/path_spec.rb
@@ -13,4 +13,22 @@ RSpec.describe Path, type: :model do
 		expect(path.included_or_parent_of?(child)).to be(true)
 		expect(path.included_or_parent_of?(children)).to be(true)
 	end
+
+	it "should return true if child_of?(path) is called on path's parent" do
+		path = Path.depth(3).sample
+		parent = path.parent
+		expect(path.child_of?(parent)).to be(true)
+	end
+
+	it "should return true if included_or_child_of?(paths) is called on a path that's a parent of a path's element" do
+		parents = Path.depth(3).sample(5)
+		child = parents.sample.direct_children.sample
+		expect(child.included_or_child_of?(parents)).to be(true)
+	end
+
+	it "should return true if included_or_child_of?(paths) is called on a path that is included in paths" do
+		paths = Path.depth(3).sample(5)
+		path = paths.sample
+		expect(path.included_or_child_of?(paths)).to be(true)
+	end
 end
