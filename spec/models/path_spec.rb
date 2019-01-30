eefc38211dea679110480f86e869d3cab40a3770
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2019-01-30 16:37:12 +0100

    Corrects more syntax errors

--- spec/models/path_spec.rb
+++ spec/models/path_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Path, type: :model do
 	it "should return that nil is not included or parent of instance" do
 		path = Path.all.sample
-		expect(path.included_or_parent_of?(nil).to be(false)
+		expect(path.included_or_parent_of?(nil)).to be(false)
 	end
 
 	it "should return correct value if asked if instance is included or parent of array or single instance" do
@@ -11,6 +11,6 @@ RSpec.describe Path, type: :model do
 		children = path.direct_children
 		child = children.first
 		expect(path.included_or_parent_of?(child)).to be(true)
-		expect(path.included_or_parent_of?(children).to be(true)
+		expect(path.included_or_parent_of?(children)).to be(true)
 	end
 end
