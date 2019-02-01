cd117c813788a567530d93ff73bb99a867056347
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2019-02-01 14:20:53 +0100

    First simple form and actions to prepare for termlists import

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,22 @@
 class StaticPagesController < ApplicationController
   def menu
   end
+
+	def import_termlists_select
+	end
+
+	def import_termlists_submit
+		file_array = []
+		params.dig(:termlists, :termlist_files).each do |file_path|
+			file = file_path.tempfile
+			if File.extname(file) == ".rb"
+				file = File.open file_path.tempfile
+				file_array << file unless file.blank?
+			else
+				flash[:warning] = "Unsupported file formats detected. Only .rb files are supported."
+			end
+		end
+		flash[:success] = "Uploaded #{file_array.count} files." unless file_array.blank?
+		redirect_to import_termlists_select_path
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,5 @@
 Rails.application.routes.draw do
   
-  namespace :admin do
-      resources :museum_objects
-      resources :excavation_sites
-      resources :material_specifieds_koo_specs
-      resources :museums
-      resources :storages
-      resources :storage_locations
-      resources :termlist_acquisition_delivered_bies
-      resources :termlist_acquisition_kinds
-      resources :termlist_authenticities
-      resources :termlist_colors
-      resources :termlist_conservations
-      resources :termlist_dating_centuries
-      resources :termlist_dating_millennia
-      resources :termlist_dating_periods
-      resources :termlist_decorations
-      resources :termlist_decoration_colors
-      resources :termlist_decoration_techniques
-      resources :termlist_excavation_site_categories
-      resources :termlist_excavation_site_kinds
-      resources :termlist_inscription_languages
-      resources :termlist_inscription_letters
-      resources :termlist_kind_of_objects
-      resources :termlist_kind_of_object_specifieds
-      resources :termlist_materials
-      resources :termlist_material_specifieds
-      resources :termlist_preservation_materials
-      resources :termlist_preservation_objects
-      resources :termlist_production_techniques
-
-      root to: "termlist_colors#index"
-    end
   get 'termlists/index'
   get 'termlists/choose'
   get 'termlists/show'
@@ -54,6 +22,10 @@ Rails.application.routes.draw do
   get 'builds/storage_locations', to: "builds#storage_locations"
   get 'builds/excavation_site_kinds', to: "builds#excavation_site_kinds"
   get 'builds/kind_of_objects_for_spec_material_path', to: "builds#kind_of_objects_for_spec_material_path"
+
+	# importer actions
+	get  '/import_termlists/select', to: 'static_pages#import_termlists_select'
+	post '/import_termlists/submit', to: 'static_pages#import_termlists_submit'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
