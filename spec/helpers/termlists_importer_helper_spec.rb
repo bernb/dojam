229c74d4215d4f8cf18d0e498c6ca09ad19006e8
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2019-02-15 11:18:49 +0100

    Add test for use of safe_load for termlist import

--- spec/helpers/termlists_importer_helper_spec.rb
+++ spec/helpers/termlists_importer_helper_spec.rb
@@ -2,5 +2,12 @@ require "rails_helper"
 
 RSpec.describe TermlistsImporterHelper, type: :helper do
 	describe "#parse_import_file" do
+    it "should use psych's safe_load method to load yaml files" do
+      file = fixture_file_upload "#{Rails.root}/spec/fixtures/files/yaml/malicious_file.yaml"
+      expect{helper.parse_import_files [file]}.to raise_error(Psych::DisallowedClass)
+    end
+    it "should disallow files that are not .yaml files"
+    it "should disallow files with invalid terms"
+    it "should allow to import a correctly formatted yaml file with only whitelisted entries"
 	end
 end
