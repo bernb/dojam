3b5ee9a458976ced22d2a8d380e46cb8adcf71a6
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2019-03-08 11:47:13 +0100

    Add factory for pathless museum object

--- spec/factories/museum_objects.rb
+++ spec/factories/museum_objects.rb
@@ -2,4 +2,8 @@ FactoryBot.define do
   factory :museum_object do
 		inv_number { "T.1234" }
   end
+
+  factory :pathless_museum_object, class: :museum_object do
+    inv_number { "T.0000" }
+  end
 end
