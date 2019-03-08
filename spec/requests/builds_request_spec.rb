57bbfb0b683f948f1c18b134e98d0539b97b09bb
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2019-03-08 13:25:11 +0100

    Remove now obsolete request spec

--- spec/requests/builds_request_spec.rb
+++ spec/requests/builds_request_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "Viewing or editing museum_objects", type: :request do
-  context "when no main path is set" do
-    before(:each) do
-      @museum_object = create(:pathless_museum_object)
-    end
-    it "should move back if dependent step/view was choosen by user" do
-      get museum_object_build_path(@museum_object.id, :step_production)
-      expect(response).to redirect_to(
-                                 museum_object_build_path(@museum_object.id, :step_confirm))
-    end
-  end
-end
+# Kept to demonstrate how a request spec would work
+#RSpec.describe "Viewing or editing museum_objects", type: :request do
+#  context "by default show requested step" do
+#    before(:each) do
+#      @museum_object = create(:museum_object)
+#    end
+#    it "should move back if dependent step/view was choosen by user" do
+#      get museum_object_build_path(@museum_object.id, :step_production)
+#      expect(response).to redirect_to(
+#                                 museum_object_build_path(@museum_object.id, :step_production))
+#    end
+#  end
+#end
