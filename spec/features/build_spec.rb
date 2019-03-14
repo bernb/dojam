144bdc85694e840fdc6a612a3a9df4c1ef648ebf
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2019-03-14 16:44:09 +0100

    Test start checking default route for material - fails in this commit

--- spec/features/build_spec.rb
+++ spec/features/build_spec.rb
@@ -11,6 +11,24 @@ RSpec.feature "Add new object steps", js: true, type: :feature do
     visit '/museum_objects/new'
     select('hall A', from: 'storage_storage_id')
     select('showcase A1', from: 'museum_object_storage_location_id')
+    fill_in('inventory number of museum', with: 'T.1111')
+    click_button('next step')
+    expect(page).to have_text("acquisition")
+
+    click_button('next step')
+    expect(page).to have_text("provenance")
+
+    click_button('next step')
+    expect(page).to have_text("material")
+    undet_path_id = Path.undetermined_path.to_depth(1).id
+    expect(page).to have_selector(
+      "#museum_object_secondary_path_ids_" + undet_path_id.to_s)
+
+    click_button('next step')
+    expect(page).to have_text("material specified")
+    undet_path_id = Path.undetermined_path.to_depth(2).id
+    expect(page).to have_selector(
+      "#museum_object_secondary_path_ids_" + undet_path_id.to_s)
   end
 
 end
