29a54d092c16b613b7056cd13965db2775129317
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2019-04-05 10:13:30 +0200

    Rebuild release script for use with docker. Not complete yet

--- scripts/create_new_release.sh
+++ scripts/create_new_release.sh
@@ -1,4 +1,29 @@
-git checkout master
-git merge --no-ff development 
-git push
-git checkout development
+#!/bin/bash
+
+cd staging-area
+
+git pull
+
+# Grep latest db backup
+latest_archive="$(borg list --short --last 1)"
+borg -p extract --strip-components 3 ::"$latest_archive"
+cp ../app/config/secrets.yml config/
+
+docker-compose build
+docker-compose up
+
+cd app/
+RAILS_ENV=staging rails rspec spec
+# Show test-result and wait for confirmation
+# Grep latest version from version file
+# Increment version and write back to file
+# Merge staging into master
+# Move back to development branch
+
+# Use parameters to determine if release is only to be tested or
+# should be merged into master for real
+
+#git checkout master
+#git merge --no-ff development 
+#git push
+#git checkout development
