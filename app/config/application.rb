abc6dc8858a4555054cc6d811980a7b282d00ad7
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2019-07-04 19:04:46 +0200

    Use :webkit for feature test in all environments as we use production env for CD testing

--- app/config/application.rb
+++ app/config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+Capybara.javascript_driver = :webkit
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
