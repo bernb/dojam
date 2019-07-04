bd074cad883533625bbef9f8f2536e750c61f523
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2019-07-04 19:14:14 +0200

    Remove wrong entry from last commit

--- scripts/create_new_release.sh
+++ scripts/create_new_release.sh
@@ -13,13 +13,24 @@ fi
 
 # Grep latest db backup
 latest_archive="$(borg list --short --last 1)"
-borg -p extract --strip-components 3 ::"$latest_archive"
-cp ../app/config/secrets.yml config/
+#borg -p extract --strip-components 3 ::"$latest_archive"
 
-docker-compose build
-docker-compose up
+# Copy secrets
+cp ../app/config/secrets.yml app/config/
+cp ../dojam-postgres.env .
 
-docker-compose exec db dropdb -U dojam DOJAM_DB
-docker-compose exec db createdb -U dojam DOJAM_DB
-docker-compose exec db pg_restore -U dojam -d DOJAM_DB /var/lib/postgresql/data/dojam.dump
-docker-compose exec app xvfb-run -a bundle exec rspec
+# Check if an instance is still running and stop it
+container_count=$(sudo docker-compose ps | grep jamapp | wc -l)
+if [ "$container_count" -gt 0 ]; then
+	sudo docker-compose down
+fi
+
+sudo docker-compose build
+sudo docker-compose up 
+
+sudo docker-compose exec db dropdb -U dojam DOJAM_DB
+sudo docker-compose exec db createdb -U dojam DOJAM_DB
+sudo docker-compose exec db pg_restore -U dojam -d DOJAM_DB /var/lib/postgresql/data/dojam.dump
+sudo docker-compose exec app rails db:migrate
+sudo docker-compose exec --env RAILS_ENV=development app xvfb-run -a bundle exec rspec
+sudo docker-compose down
