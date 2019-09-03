db80079d2cb18c4bf7028787b2ab021d92d99539
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2019-09-03 11:55:52 +0200

    Add museum_objects methods for koo and koos

--- app/app/models/kind_of_object.rb
+++ app/app/models/kind_of_object.rb
@@ -3,6 +3,17 @@ class KindOfObject < Termlist
 		3
 	end
 
+  def museum_objects
+    paths = Path.depth(3)
+      .last_id(self.id)
+      .map(&:direct_children)
+      .flatten
+      .uniq
+    as_main = MuseumObject.where(main_path: paths)
+    as_secondary = paths.map(&:museum_objects).flatten.uniq
+    return as_main + as_secondary
+  end
+
 	def kind_of_object_specifieds material_specified: 
 		ms_id = material_specified.id
 		paths = Path.where("path SIMILAR TO ?", "/\\d{1,}/#{ms_id}/#{self.id}/%")
--- app/app/models/kind_of_object_specified.rb
+++ app/app/models/kind_of_object_specified.rb
@@ -1,5 +1,13 @@
 class KindOfObjectSpecified < Termlist
-	def depth
+  def depth
 		4
 	end
+  
+  def museum_objects
+    paths = Path.depth(4)
+      .last_id(self.id)
+    as_main = MuseumObject.where(main_path: paths)
+    as_secondary = paths.map(&:museum_objects).flatten.uniq
+    return as_main + as_secondary
+  end
 end
