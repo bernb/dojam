625369406336336ee15d62700e43334a7f63ebe7
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2019-09-04 11:18:38 +0200

    Add museum_objects method for dating_century and millennium, which gives back the sum of *_begin and *_end in corresponding museum_objects model

--- app/app/models/dating_century.rb
+++ app/app/models/dating_century.rb
@@ -1,6 +1,12 @@
 class DatingCentury < Termlist
-  has_many :museum_objects
+  has_many :museum_objects_as_begin, foreign_key: 'dating_century_begin', class_name: 'MuseumObject'
+  has_many :museum_objects_as_end, foreign_key: 'dating_century_end', class_name: 'MuseumObject'
 	acts_as_list scope: [:type]
+
+  def museum_objects
+    museum_objects_as_begin.or(museum_objects_as_end)
+  end
+
 	def self.is_independent_of_paths
 		true
 	end
