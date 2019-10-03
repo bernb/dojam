dfd2ccaa1521edaa5b5e16f095151776717e89bb
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2019-10-03 08:25:19 +0200

    Ajax call to get new form search element

--- app/app/assets/javascripts/museum_objects.js
+++ app/app/assets/javascripts/museum_objects.js
@@ -4,7 +4,17 @@ ready = function() {
   
   $("tr[data-link]").click(function() { // Used in search result table / index view
     window.location = $(this).data("link")
-  })
+  });
+
+	$(".form-search-add").click(function(event) {
+		event.preventDefault();
+		$.ajax({
+			url: 'add_search_field',
+			type: 'GET',
+			dataType: 'script'
+		});
+	});
+
 };
 
 $(document).on('turbolinks:load', ready);
