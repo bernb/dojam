601a056f76b199c301ea00f92ac4d7b9a943d67e
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2020-05-12 15:14:26 +0200

    Admin: Add museum object informations to material

--- app/app/admin/materials.rb
+++ app/app/admin/materials.rb
@@ -12,5 +12,25 @@ ActiveAdmin.register Material do
     column :material_specifieds
     column :created_at
     column :updated_at
+    column :museum_object_count do |m|
+      m.museum_objects.count
+    end
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row :created_at
+      row :updated_at
+      row :museum_objects do |m|
+        @m_objects = Kaminari.paginate_array(m.museum_objects).page(params[:page]).per(50)
+        result = @m_objects.map{|m| link_to m.decorate.full_inv_number, admin_museum_object_path(m)}
+      end
+      row :page do |p|
+        [page_entries_info(@m_objects), paginate(@m_objects)]
+      end
+    end
+    active_admin_comments
   end
 end
