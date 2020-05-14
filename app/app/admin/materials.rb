dc186c6618b08a7567f2e1bfc4381eb3bffcc3bc
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2020-05-14 18:38:25 +0200

    Admin: Some work on material views and form

--- app/app/admin/materials.rb
+++ app/app/admin/materials.rb
@@ -21,8 +21,7 @@ ActiveAdmin.register Material do
     attributes_table do
       row :id
       row :name
-      row :created_at
-      row :updated_at
+      row :material_specifieds
       row :museum_objects do |m|
         @m_objects = Kaminari.paginate_array(m.museum_objects).page(params[:page]).per(50)
         result = @m_objects.map{|m| link_to m.decorate.full_inv_number, admin_museum_object_path(m)}
@@ -30,7 +29,19 @@ ActiveAdmin.register Material do
       row :page do |p|
         [page_entries_info(@m_objects), paginate(@m_objects)]
       end
+      row :created_at
+      row :updated_at
     end
     active_admin_comments
+  end
+
+  form do |f|
+    inputs 'material details' do
+      @mo = Material.find params[:id]
+      f.semantic_errors
+      input :name 
+      input :paths, as: :check_boxes, label: "material specifieds", collection: @mo.paths.reject{|p| p.depth != 2}.map{|p| [p.last_object_name, p.id]}
+    end
+    f.actions
   end
 end
