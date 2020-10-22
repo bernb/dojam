ceed8525f864c32faa3a049a1e6d1b07b58879aa
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2020-10-22 15:08:17 +0200

    Move devise user model method to where they belong

--- app/app/controllers/users_controller.rb
+++ app/app/controllers/users_controller.rb
@@ -1,12 +1,4 @@
 class UsersController < ApplicationController
-  def active_for_authentication?
-    super && is_enabled?
-  end
-
-  def inactive_message
-    is_enabled? ? super : t('your account is not yet enabled')
-  end
-
   def switch_locale
     locale = params[:locale]
     if locale.in? ["ar", "en"]
