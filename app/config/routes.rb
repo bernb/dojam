7ad07c1750ab09a2c51d9b6e869251313769cb82
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2020-10-22 15:07:51 +0200

    Remove admin_users routes as model was removed completely from the app

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   
   get 'users/index'
   devise_for :users
-  devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 #  get 'termlists/index'
 #  get 'termlists/choose'
