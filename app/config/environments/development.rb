86f79ca2604358767ae62bc1d10ea3cc8fc63a3d
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2020-10-26 11:10:30 +0100

    Add config to send emails from dojam over gmail account

--- app/config/environments/development.rb
+++ app/config/environments/development.rb
@@ -45,9 +45,21 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'localhost:22333' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      tls: false,
+      authentication: :plain,
+      enable_starttls_auto: true,
+      user_name: Rails.application.credentials[:email][:user_name],
+      password: Rails.application.credentials[:email][:password],
+      openssl_verify_mode: 'none'
+  }
 
-  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
