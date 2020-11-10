d7048e7ffeec7f8f6b4745f0c8b3202a250e3892
bernb <15048930+bernb@users.noreply.github.com>
2020-11-10 09:18:59 +0100

    Add rails-i18n which imports some basic translations and formats used by activeadmin

--- app/config/environments/development.rb
+++ app/config/environments/development.rb
@@ -44,6 +44,9 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = false
 
+  # Only load needed translations
+  config.i18n.available_locales = [:en, :ar]
+
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
