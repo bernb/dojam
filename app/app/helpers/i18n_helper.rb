0b7dfc5033ed955612f2b8c5076481ff61a3c8a9
bernb <15048930+bernb@users.noreply.github.com>
2020-11-15 13:16:03 +0100

    In I18nHelper allow empty string as key and return empty as value. This avoids throwing an error if key is an empty string.

--- app/app/helpers/i18n_helper.rb
+++ app/app/helpers/i18n_helper.rb
@@ -1,5 +1,8 @@
 module I18nHelper
   def translate(key, options={})
+    if key == ""
+      return ""
+    end
     super(key, options.merge(raise: true))
   rescue I18n::MissingTranslationData
     key
