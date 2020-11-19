e88f4c2a70e562cd146c4fc1805def89c378fd68
bernb <15048930+bernb@users.noreply.github.com>
2020-11-19 17:17:37 +0100

    Add loan information to export to list. Use safe navigation for other attributes to avoid errors if something is missing.

--- app/app/controllers/museum_objects_controller.rb
+++ app/app/controllers/museum_objects_controller.rb
@@ -28,7 +28,8 @@ class MuseumObjectsController < ApplicationController
     ids = params.dig(:museum_objects_export_list, :ids).presence || "[]"
     ids = JSON.parse ids
     museum_objects = MuseumObject.where(id: ids).map(&:decorate)
-    sheet.row(0).push I18n.t('museum'),
+    sheet.row(0).push I18n.t('on loan to institution'),
+             I18n.t('museum'),
                      I18n.t('inventory no. incl. extension'),
                      I18n.t('other inventory no.'),
                      I18n.t('location'),
@@ -43,21 +44,22 @@ class MuseumObjectsController < ApplicationController
                      I18n.t('description')
     museum_objects.each_with_index do |m, i|
       row = sheet.row(i+1)
-      row.push m.museum.name,
+      row.push m.loan_out&.to_institution || "",
+               m.museum.name,
                m.full_inv_number,
                m.inv_numberdoa,
-               m.storage.name,
+               m.storage&.name,
                m.storage_location.name,
                m.excavation_site.name,
-               m.main_material.name,
-               m.main_material_specified.name,
-               m.kind_of_object.name,
-               m.kind_of_object_specified.name,
-               m.preservation_object.name,
-               m.dating_period.name,
+               m.main_material&.name,
+               m.main_material_specified&.name,
+               m.kind_of_object&.name,
+               m.kind_of_object_specified&.name,
+               m.preservation_object&.name,
+               m.dating_period&.name,
                # More than 30k chars / 32kB will result in a corrupted file,
                # see https://github.com/zdavatz/spreadsheet/blob/master/GUIDE.md#cautionary-note-about-cell-content-length
-               m.remarks[0..30_000]
+               m.remarks&.[](0..30_000)
     end
     ExcelExporterHelper.fit_columns(sheet)
     file = StringIO.new
