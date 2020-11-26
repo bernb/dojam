d1dbc49872501db1186b04e972003df2149b5801
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2020-11-26 11:26:32 +0100

    Add function to traverse project dir and replace I18n keys according to rails I18n standards

--- app/app/helpers/i18n_helper.rb
+++ app/app/helpers/i18n_helper.rb
@@ -8,4 +8,22 @@ module I18nHelper
     key
   end
   alias :t :translate
+
+  module KeyTranslator
+    def self.call(path, simulate: true)
+      search_pattern = /t\(['"](.*?)['"]\)/
+      Dir.chdir(path)
+      files = Dir.glob("**/")
+      files.each do |f|
+        File.eachline.with_index(f) do |l, i|
+          result = l.scan(search_pattern)
+          result.each do |r|
+            puts "File #{File.basename(f)} in line #{i.to_s}, old/new:"
+            puts l
+            puts l.sub(r, transform_key(r))
+          end
+        end
+      end
+    end
+  end
 end
