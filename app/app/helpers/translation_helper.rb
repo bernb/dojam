1868f717bd60580c3bdcb1331eda9397c134ddc7
bernb <15048930+bernb@users.noreply.github.com>
2020-11-26 11:35:07 +0100

    Refactor: Move I18n Key transformer into correct helper module

--- app/app/helpers/translation_helper.rb
+++ app/app/helpers/translation_helper.rb
@@ -10,4 +10,20 @@ module TranslationHelper
                .map(&:squish)
                .map{|e| e.gsub(/\s/, '_')}
   end
+
+  def self.transform_keys(path, simulate: true)
+    search_pattern = /t\(['"](.*?)['"]\)/
+    Dir.chdir(path)
+    files = Dir.glob("**/")
+    files.each do |f|
+      File.eachline.with_index(f) do |l, i|
+        result = l.scan(search_pattern)
+        result.each do |r|
+          puts "File #{File.basename(f)} in line #{i.to_s}, old/new:"
+          puts l
+          puts l.sub(r, transform_key(r))
+        end
+      end
+    end
+  end
 end
