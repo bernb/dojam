537b36bad22d1677a9c6602f0f267c4edee44cf2
bernb <15048930+bernb@users.noreply.github.com>
2020-11-26 15:05:39 +0100

    Working and tested key replacer for I18n

--- app/app/helpers/translation_helper.rb
+++ app/app/helpers/translation_helper.rb
@@ -6,22 +6,23 @@ module TranslationHelper
   def self.clean_collection(array)
     return array
                .map(&:downcase)
-               .map{|e| e.gsub(/[^a-z\s]/, "")}
-               .map(&:squish)
-               .map{|e| e.gsub(/\s/, '_')}
+               .map{|e| e.gsub(/[\/\-\\]/, "_")} # replace some chars used as seperators with underscore
+               .map{|e| e.gsub(/[^0-9a-z_\s]/, "")} # remove everything except a-z and whitespace and underscore
+               .map(&:squish) # remove trailing / leading underscores
+               .map{|e| e.gsub(/\s/, '_')} # replace all remaining underscores with a whitespace
   end
 
   def self.transform_keys(path, simulate: true)
-    search_pattern = /t\(['"](.*?)['"]\)/
+    search_pattern = /(?<=[^a-z]t\(['"]).*?(?=['"]\))/
     Dir.chdir(path)
-    files = Dir.glob("**/")
+    files = Dir.glob(%w[**/*.rb **/*.erb])
     files.each do |f|
-      File.eachline.with_index(f) do |l, i|
+      File.open(f).each_with_index do |l, i|
         result = l.scan(search_pattern)
         result.each do |r|
           puts "File #{File.basename(f)} in line #{i.to_s}, old/new:"
           puts l
-          puts l.sub(r, transform_key(r))
+          puts l.sub(r, self.clean_str(r))
         end
       end
     end
