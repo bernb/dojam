b353a38bb25a437cae9b8f8f8d4c5dabf35f99d8
bernb <15048930+bernb@users.noreply.github.com>
2020-12-08 16:34:47 +0100

    Refactor: Move Excel-Exporter into ExcelExporterHelper

--- app/app/helpers/excel_exporter_helper.rb
+++ app/app/helpers/excel_exporter_helper.rb
@@ -11,4 +11,45 @@ module ExcelExporterHelper
     end
     return sheet
   end
+
+  def self.create_sheet(ids)
+    export = Spreadsheet::Workbook.new
+    sheet = export.create_worksheet
+    museum_objects = MuseumObject.where(id: ids).map(&:decorate)
+    sheet.row(0).push I18n.t('on_loan_to_institution'),
+                      I18n.t('museum'),
+                      I18n.t('inventory_no_incl_extension'),
+                      I18n.t('other_inventory_no'),
+                      I18n.t('location'),
+                      I18n.t('detailed_location'),
+                      I18n.t('site_name'),
+                      I18n.t('material'),
+                      I18n.t('material_specified'),
+                      I18n.t('kind_of_object'),
+                      I18n.t('kind_of_object_specified'),
+                      I18n.t('preservation_of_object'),
+                      I18n.t('dating_period'),
+                      I18n.t('description')
+    museum_objects.each_with_index do |m, i|
+      row = sheet.row(i+1)
+      row.push m.loan_out&.to_institution || "",
+               m.museum.name,
+               m.full_inv_number,
+               m.inv_numberdoa,
+               m.storage&.name,
+               m.storage_location.name,
+               m.excavation_site.name,
+               m.main_material&.name,
+               m.main_material_specified&.name,
+               m.kind_of_object&.name,
+               m.kind_of_object_specified&.name,
+               m.preservation_object&.name,
+               m.dating_period&.name,
+               # More than 30k chars / 32kB will result in a corrupted file,
+               # see https://github.com/zdavatz/spreadsheet/blob/master/GUIDE.md#cautionary-note-about-cell-content-length
+               m.remarks&.[](0..30_000)
+    end
+    self.fit_columns(sheet)
+    return export
+  end
 end
