4647646baf5fd0d3a89c078759cd5778ea67ff3b
bernb <15048930+bernb@users.noreply.github.com>
2021-03-25 15:29:17 +0100

    Fix Bug: Excavation sites not shown in the correct order
    We always sort by name, but put 'undetermined' at the buttom, which is achieved by providing a default_scope for the abstract class Termlist. The ExcavationSite class however does not inherit from this class, so the proper default_scope was added now.

--- app/app/models/excavation_site.rb
+++ app/app/models/excavation_site.rb
@@ -1,5 +1,6 @@
 class ExcavationSite < ApplicationRecord
   has_many :museum_objects, inverse_of: :excavation_site, dependent: :restrict_with_error
+  default_scope {order(Arel.sql("excavation_sites.name = 'undetermined'"), name: :asc)}
 
 	def self.is_independent_of_paths
 		true
