34bc90d14ff96b7f47f058bfff5cb8af0b7ccbad
bernb <15048930+bernb@users.noreply.github.com>
2021-04-30 17:29:21 +0200

    Change: Use create_or_find_by instead of create as a first step to be more robust and idempotence

--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -1,6 +1,6 @@
-storage_location = StorageLocation.create name_en: "undefined"
-storage = Storage.create name_en: "undefined"
-museum = Museum.create name: "undefined"
+storage_location = StorageLocation.create_or_find_by name_en: "undefined"
+storage = Storage.create_or_find_by name_en: "undefined"
+museum = Museum.create_or_find_by name: "undefined"
 storage.storage_locations << storage_location
 museum.storages << storage
 
@@ -30,18 +30,19 @@ term_categories = ["AcquisitionDeliveredBy",
 
 
 term_categories.each do |term_category|
-  term_category.constantize.create name_en: "undetermined"
+  term_category.constantize.create_or_find_by name_en: "undetermined"
 end
 
 m = Material.first
 ms = MaterialSpecified.first
 koo = KindOfObject.first
 koos = KindOfObjectSpecified.first
-pp Path.all
-m.paths   << Path.create(path: "/#{m.id}")
-ms.paths  << Path.create(path: "/#{m.id}/#{ms.id}")
-koo.paths << Path.create(path: "/#{m.id}/#{ms.id}/#{koo.id}")
-undetermined_path = Path.create(path: "/#{m.id}/#{ms.id}/#{koo.id}/#{koos.id}")
+
+# ToDo: Material overrides paths method for some reason. Change that back as such is highly unexpecting and bad. There also exists an after_create callback in Termlist model.
+#m.paths   << Path.create_or_find_by(path: "/#{m.id}")
+ms.paths  << Path.create_or_find_by(path: "/#{m.id}/#{ms.id}")
+koo.paths << Path.create_or_find_by(path: "/#{m.id}/#{ms.id}/#{koo.id}")
+undetermined_path = Path.create_or_find_by(path: "/#{m.id}/#{ms.id}/#{koo.id}/#{koos.id}")
 koos.paths << undetermined_path
 
 term_categories.each do |term_category|
