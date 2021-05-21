6f247e211f0f7dcec9090bad4d53d5a7bf2f4797
bernb <15048930+bernb@users.noreply.github.com>
2021-05-21 10:55:20 +0200

    Add path dependent terms to file export

--- app/app/helpers/file_export_helper.rb
+++ app/app/helpers/file_export_helper.rb
@@ -3,10 +3,25 @@ module FileExportHelper
   def self.call
     ms = MaterialSpecified.find_by name_en: "marble"
     ms_hash = material_specified_to_hash ms
-    YAML.dump ms_hash
+    path_dependent_term_hash = path_dependent_terms_to_hash ms
+    YAML.dump ms_hash.merge(path_dependent_term_hash)
   end
 
   private
+
+  def self.path_dependent_terms_to_hash ms
+    term_hash = {}
+    # ToDo: Why is rejecting koos neccessary?
+    term_types = Termlist.existing_typenames.reject{|t| t == "KindOfObjectSpecified"}
+    leaf = ms.paths.first.leafs.first
+    term_types.each do |term_type|
+      external_name = Termlist.to_external_type(term_type).underscore.pluralize
+      terms = term_type.constantize.for_path(leaf).map(&:name_en)
+      term_hash[external_name] = terms unless terms.empty?
+    end
+    term_hash
+  end
+
   # We use a format that's simple as a file on the cost of a bit complex format used internally:
   # ms_hash["kind_of_objects"] is an array of hashes, those hashes only have a single key with an array as value
   # The result looks like this:
