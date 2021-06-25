52ba4e94c43b9f7524097307e176bae9be2a5053
bernb <15048930+bernb@users.noreply.github.com>
2021-06-25 17:01:14 +0200

    Show Storage and StorageLocation in Admin Backend

---
+++ app/app/admin/storage_locations.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register StorageLocation do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name_en, :storage_id, :name_ar
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name_en, :storage_id, :name_ar]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
---
+++ app/app/admin/storages.rb
@@ -0,0 +1,34 @@
+ActiveAdmin.register Storage do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :name_en, :name_ar
+
+  index do
+    column :name_en do |storage|
+      link_to storage.name_en, admin_storage_path(storage)
+    end
+    column :name_ar do |storage|
+      link_to storage.name_ar, admin_storage_path(storage) unless storage.name_ar.nil?
+    end
+    column :storage_locations
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :name_en
+      row :name_ar
+      row :storage_locations
+    end
+  end
+
+
+  
+end
