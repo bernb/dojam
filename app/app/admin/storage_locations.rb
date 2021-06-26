b8d69745ead0dda16c6610ab849a76034df73cdb
bernb <15048930+bernb@users.noreply.github.com>
2021-06-26 10:25:44 +0200

    Refine StorageLocation model in admin backend

--- app/app/admin/storage_locations.rb
+++ app/app/admin/storage_locations.rb
@@ -5,14 +5,35 @@ ActiveAdmin.register StorageLocation do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name_en, :storage_id, :name_ar
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name_en, :storage_id, :name_ar]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params :name_en, :storage_id, :name_ar
+  remove_filter :museum_objects
+
+  index do
+    column :name_en do |storage|
+      link_to storage.name_en, admin_storage_location_path(storage)
+    end
+    column :name_ar do |storage|
+      link_to storage.name_ar, admin_storage_location_path(storage) unless storage.name_ar.nil?
+    end
+    column :storage
+    column :museum_objects do |storage|
+      storage.museum_objects.count
+    end
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :name_en
+      row :name_ar
+      row :storage
+      row :museum_objects do |storage|
+        storage.museum_objects.count
+      end
+    end
+  end
   
 end
