67207be4300515d137867c1531920cc6221d2137
bernb <15048930+bernb@users.noreply.github.com>
2021-06-26 10:41:28 +0200

    Refine MuseumObject model in admin backend

--- app/app/admin/museum_objects.rb
+++ app/app/admin/museum_objects.rb
@@ -1,9 +1,20 @@
 ActiveAdmin.register MuseumObject do
   filter :inv_number
+  filter :storage_location
+  filter :excavation_site, multiple: true
+
+
   index do
     column :full_inv_number do |mo|
       link_to mo.decorate.full_inv_number, admin_museum_object_path(mo)
     end
+    column :storage
+    column :storage_location
+    column :main_material
+    column :main_material_specified
+    column :kind_of_object
+    column :kind_of_object_specified
+    actions
   end
 
   show do
