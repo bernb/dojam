bfacca39c89f02093b01ae989e00f849e3fb76bd
bernb <15048930+bernb@users.noreply.github.com>
2021-06-26 10:24:38 +0200

    Add method to Storage model: List associated museum objects

--- app/app/models/storage.rb
+++ app/app/models/storage.rb
@@ -2,4 +2,8 @@ class Storage < ApplicationRecord
   translates :name
   belongs_to :museum
   has_many :storage_locations, dependent: :restrict_with_error
+
+  def museum_objects
+    storage_locations.map(&:museum_objects).flatten.uniq
+  end
 end
