3b27dcf9e406ee666fc26a43bf1eb0c2b76bfae0
bernb <15048930+bernb@users.noreply.github.com>
2021-09-24 11:18:44 +0200

    Refactor: Add explanations to StringExtension

--- app/lib/string_extensions.rb
+++ app/lib/string_extensions.rb
@@ -1,8 +1,11 @@
 module StringExtensions
+  # Original signature of the method:
+  # humanize(capitalize: true, keep_id_suffix: false)
   def humanize(capitalize: false, keep_id_suffix: false)
     super(capitalize: capitalize, keep_id_suffix: keep_id_suffix)
   end
 
+  # Add a downcase parameter, as we want downcase most of the time even for titles
   def titleize(keep_id_suffix: false, downcase: true)
     result = super(keep_id_suffix: keep_id_suffix)
     if downcase
