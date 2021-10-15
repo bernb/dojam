0144c2dc3e60dbd27196358ecad1e1874528cc09
bernb <15048930+bernb@users.noreply.github.com>
2021-10-15 18:15:40 +0200

    Configure capybara for E2E testing
    We default to headless chrome without sandbox as it get started with root permissions within the docker context

---
+++ app/spec/support/capybara.rb
@@ -0,0 +1,14 @@
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :chrome_headless
+  end
+end
+
+Capybara.register_driver :chrome_headless do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
