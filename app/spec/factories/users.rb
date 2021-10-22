ec29f00c39da18045309d6e6cff5d2abd980295a
bernb <15048930+bernb@users.noreply.github.com>
2021-10-22 16:42:06 +0200

    Add user factories

--- app/spec/factories/users.rb
+++ app/spec/factories/users.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :user do
-    
+    email {"user@test.com"}
+    password {"password"}
+    is_enabled {true}
+
+    factory :ext_user do
+      has_extended_access {true}
+    end
+
+    factory :main_admin do
+      email {"main_admin@test.com"}
+      has_extended_access {true}
+      id {1}
+    end
   end
 end
