812c9df5c367a502e537d22142b8c5a836d52773
bernb <15048930+bernb@users.noreply.github.com>
2021-10-22 13:55:20 +0200

    Fix Bug: Correct method call for koo in model test

--- app/spec/models/kind_of_object_spec.rb
+++ app/spec/models/kind_of_object_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe KindOfObject, type: :model  do
   it 'should show unique entries for specified kind of object' do
-    kind_of_object = KindOfObject.find_by name: 'undetermined'
-    material_specified = MaterialSpecified.find_by name: 'undetermined'
+    kind_of_object = KindOfObject.find_by name_en: 'undetermined'
+    material_specified = MaterialSpecified.find_by name_en: 'undetermined'
     specified_list = kind_of_object.kind_of_object_specifieds(
       material_specified: material_specified)
     specified_names = specified_list.map(&:name)
