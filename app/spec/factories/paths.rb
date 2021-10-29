aae2d1f54bd702936313860baf8bc9bf5adb3689
bernb <15048930+bernb@users.noreply.github.com>
2021-10-29 16:40:23 +0200

    Fix syntax errors

--- app/spec/factories/paths.rb
+++ app/spec/factories/paths.rb
@@ -1,15 +1,17 @@
 FactoryBot.define do
-  termlist {nil}
+  factory :path do
+    termlist {nil}
 
-  transient do
-    term_id {nil}
-    term_ids {nil}
-  end
+    transient do
+      term_id {nil}
+      term_ids {nil}
+    end
 
-  if term_id.present?
-    path {"/#{term_id}"}
-  end
-  if term_ids.present?
-    path {term_ids.map{|n| "/#{n}"}.reduce(:+)}
+    if term_id.present?
+      path {"/#{term_id}"}
+    end
+    if term_ids.present?
+      path {term_ids.map{|n| "/#{n}"}.reduce(:+)}
+    end
   end
 end
--- app/spec/factories/termlists.rb
+++ app/spec/factories/termlists.rb
@@ -1,17 +1,17 @@
 FactoryBot.define do
-  factory :material, aliases: :m do
+  factory :material, aliases: [:m] do
     sequence(:name_en) {|n| "m_#{n}"}
   end
 
-  factory :material_specified, aliases: :ms do
+  factory :material_specified, aliases: [:ms] do
     sequence(:name_en) {|n| "ms_#{n}"}
   end
 
-  factory :kind_of_object, aliases: :koo do
+  factory :kind_of_object, aliases: [:koo] do
     sequence(:name_en) {|n| "koo_#{n}"}
   end
 
-  factory :kind_of_object_specified, aliases: :koos do
+  factory :kind_of_object_specified, aliases: [:koos] do
     sequence(:name_en) {|n| "koos_#{n}"}
   end
 end
