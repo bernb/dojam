0fd236966051539cdde981b22e2c32a3f7dbea34
bernb <15048930+bernb@users.noreply.github.com>
2021-10-29 15:24:14 +0200

    Cleanup empty spec factories

--- app/spec/factories/decoration_technique_museum_objects.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :decoration_technique_museum_object do
-    museum_object { nil }
-  end
-end
--- app/spec/factories/production_technique_museum_objects.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :production_technique_museum_object do
-    museum_object { nil }
-    production_technique { nil }
-  end
-end
