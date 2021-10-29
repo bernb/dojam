8b77ea9a0e7a13e569d622095d4c362bcdc1613a
bernb <15048930+bernb@users.noreply.github.com>
2021-10-29 16:05:00 +0200

    Add factories for paths and termlists

---
+++ app/spec/factories/paths.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  termlist {nil}
+
+  transient do
+    term_id {nil}
+    term_ids {nil}
+  end
+
+  if term_id.present?
+    path {"/#{term_id}"}
+  end
+  if term_ids.present?
+    path {term_ids.map{|n| "/#{n}"}.reduce(:+)}
+  end
+end
