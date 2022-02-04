4fad689160945ea0b285d8f11d203d491f367550
Bernard Beitz <Bernard.Beitz@mailbox.org>
2022-02-04 15:03:20 +0100

    Do not live compile assets with sprockets in tests
    Live compiling lead to very slow system tests with docker.

--- app/config/environments/test.rb
+++ app/config/environments/test.rb
@@ -44,6 +44,10 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+  # This takes sprockets out of the loop as only precompiled assets are used.
+  # This is way faster for docker containers as dynamically caching/compiling needs
+  # a lot of disk read/writes.
+  # However you might need to invoke `RAILS_ENV=test rake assets:precompile` if assets
+  # are not precompiled now.
+  config.assets.compile = false
 end
