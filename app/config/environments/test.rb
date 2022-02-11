048fc285239c348fbebe16fdfa92d92ce9df210e
Bernard Beitz <Bernard.Beitz@mailbox.org>
2022-02-11 18:55:37 +0100

    Use custom javascript driver
    The default drivers do not work with docker

--- app/config/environments/test.rb
+++ app/config/environments/test.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  Capybara.javascript_driver = :selenium_headless
+  Capybara.javascript_driver = :chrome_headless
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
