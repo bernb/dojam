30d688e0dd43c39be47ea7ebabcff7535ab6ad09
Bernard Beitz <Bernard.Beitz@mailbox.org>
2022-02-18 18:07:55 +0100

    Add factory mo_at_step_acquisition
    This sets properties that were required in earlier steps or might be present at the current step.

--- app/spec/factories/museum_objects.rb
+++ app/spec/factories/museum_objects.rb
@@ -1,10 +1,14 @@
 FactoryBot.define do
+  sequence(:inv_number) { |n| "T.#{n}"}
   factory :museum_object do
+    trait :step_museum_complete do
+      inv_number    { generate(:inv_number) }
+      storage_location
+    end
 
+    factory :mo_at_step_acquisition, traits: [:step_museum_complete]
   end
-  # sequence :inv_number do |n|
-  #   ("T.1"..).each
-  # end
+
   #
   # factory :museum_object do
   #   inv_number
