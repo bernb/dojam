f04f67acbf103d50315f3063cd3726f725265446
Bernard Beitz <Bernard.Beitz@mailbox.org>
2022-02-18 15:20:44 +0100

    Working system tests for step_museum

--- app/spec/system/build_step_museum_spec.rb
+++ app/spec/system/build_step_museum_spec.rb
@@ -1,33 +1,31 @@
 require 'rails_helper'
 
-feature "Add a new object", type: :system do
-  feature "step_museum", type: :system do
+feature "Add a new object" do
+  feature "step_museum", type: :system, js: true do
+    let!(:museum_object)   { create(:museum_object) }
+    let!(:museum)          { create(:JAM)}
+    let!(:storage1)        { create :storage, museum: museum }
+    let!(:storage2)        { create(:storage, museum: museum) }
     before(:each) do
-      @museum_object = create(:museum_object)
-      @museum = create(:JAM)
-      @storage1 = create(:storage, museum: @museum)
-      @storage2 = create(:storage, museum: @museum)
-      visit "museum_objects/#{@museum_object.id}/builds/step_museum"
+      visit "/museum_objects/#{museum_object.id}/builds/step_museum"
     end
 
     scenario "correct storage options are available" do
       expect(page).to have_select("storage[storage_id]",
-                                  selected: nil,
+                                  selected: "",
                                   options: ["",
-                                            @storage1.name_en,
-                                            @storage2.name_en])
+                                            storage1.name_en,
+                                            storage2.name_en])
       expect(page).to have_select("museum_object[storage_location_id]",
-                                  selected: nil,
+                                  selected: "",
                                   options: [""])
     end
 
     scenario "user selects a storage" do
+      select storage1.name_en, from: "storage[storage_id]"
       expect(page).to have_select("museum_object[storage_location_id]",
-                                  selected: nil)
-      select @storage1.name_en, from: "storage[storage_id]"
-      expect(page).to have_select("museum_object[storage_location_id]",
-                                  selected: nil,
-                                  with_options: @storage1
+                                  selected: "",
+                                  with_options: storage1
                                                   .storage_locations
                                                   .map(&:name_en))
     end
