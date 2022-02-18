95e53669d9a4ec7ddbde8d90e2b3b4150e7e1c1f
Bernard Beitz <Bernard.Beitz@mailbox.org>
2022-02-18 18:23:41 +0100

    Add scenarios

--- app/spec/system/build_step_acquisition_spec.rb
+++ app/spec/system/build_step_acquisition_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-feature "Add a new object" do
-  feature "step_museum", type: :system, js: true do
+feature 'Add a new object' do
+  feature 'step_museum', type: :system, js: true do
     let!(:museum_object)                { create :mo_at_step_acquisition }
     let(:kind_of_acquisition_select)          { 'museum_object[acquisition_kind_id]' }
     let(:delivered_by_select)                 { 'museum_object[acquisition_delivered_by_id]' }
@@ -14,15 +14,47 @@ feature "Add a new object" do
       visit "/museum_objects/#{museum_object.id}/builds/step_acquisition"
     end
 
-    scenario "form has correct default values" do
+    scenario 'form has correct default values' do
       expect(page).to have_select(kind_of_acquisition_select,
-                                  selected: "undetermined")
+                                  selected: 'undetermined')
       expect(page).to have_select(delivered_by_select,
-                                  selected: "undetermined")
+                                  selected: 'undetermined')
       expect(page).to have_checked_field(acquisition_date_unknown_checkbox)
       expect(page).to have_select(acquisition_year_select, disabled: true)
       expect(page).to have_select(acquisition_month_select, disabled: true)
       expect(page).to have_select(acquisition_day_select, disabled: true)
+    end
+
+    scenario 'user sets acquisition date' do
+      uncheck acquisition_date_unknown_checkbox
+      expect(page).to have_select(acquisition_year_select,
+                                  disabled: false,
+                                  with_options: ['', '1900'])
+      expect(page).to have_select(acquisition_month_select,
+                                  disabled: false,
+                                  with_options: ['', '1', '12'])
+      expect(page).to have_select(acquisition_day_select,
+                                  disabled: false,
+                                  with_options: ['', '1', '31'])
+    end
+
+    scenario 'user rechecks acquisition unknown checkbox' do
+      check acquisition_date_unknown_checkbox
+      expect(page).to have_select(acquisition_year_select,
+                                  disabled: true,
+                                  selected: '')
+      expect(page).to have_select(acquisition_month_select,
+                                  disabled: true,
+                                  selected: '')
+      expect(page).to have_select(acquisition_day_select,
+                                  disabled: true,
+                                  selected: '')
+    end
+
+    scenario 'user confirms default choices' do
+      click_button 'confirm'
+      expect(page).not_to have_css '.alert-warning'
+      expect(page).to have_current_path(/step_provenance/)
     end
   end
 end
