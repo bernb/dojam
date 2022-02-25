1f2b2411524f334b727e66f9f03b952b824e93e8
Bernard Beitz <Bernard.Beitz@mailbox.org>
2022-02-25 17:20:12 +0100

    Fix Bug: Replace deprecated options parameter for selenium driver
    WARN Selenium [DEPRECATION] [:browser_options] :options as a parameter for driver initialization is deprecated. Use :capabilities with an Array of value capabilities/options if necessary instead.

--- app/spec/support/capybara.rb
+++ app/spec/support/capybara.rb
@@ -10,5 +10,5 @@ Capybara.register_driver :chrome_headless do |app|
   options.add_argument('--headless')
   options.add_argument('--no-sandbox')
 
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: [options])
 end
