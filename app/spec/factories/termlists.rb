0d14eef8e1cceb59f85e574f785ac6b9cd181870
Bernard Beitz <Bernard.Beitz@mailbox.org>
2022-03-04 15:21:16 +0100

    Use Lorem.word instead of sequences for termlist factories

--- app/spec/factories/termlists.rb
+++ app/spec/factories/termlists.rb
@@ -1,17 +1,17 @@
 FactoryBot.define do
   factory :material, aliases: [:m] do
-    sequence(:name_en) {|n| "m_#{n}"}
+    name_en { Faker::Lorem.word }
   end
 
   factory :material_specified, aliases: [:ms] do
-    sequence(:name_en) {|n| "ms_#{n}"}
+    name_en { Faker::Lorem.word }
   end
 
   factory :kind_of_object, aliases: [:koo] do
-    sequence(:name_en) {|n| "koo_#{n}"}
+    name_en { Faker::Lorem.word }
   end
 
   factory :kind_of_object_specified, aliases: [:koos] do
-    sequence(:name_en) {|n| "koos_#{n}"}
+    name_en { Faker::Lorem.word }
   end
 end
