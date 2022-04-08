7d8d6a5c89fbf806ce57e9f24af7dfe7414b968e
Bernard Beitz <Bernard.Beitz@mailbox.org>
2022-04-08 11:48:06 +0200

    Add system tests for inv. number search and fulltext search

---
+++ app/spec/system/search.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+feature "Search for museum objects", type: :system, js: true do
+  let(:museum1) { create :museum_with_storage_locations }
+  let!(:object1) { create :museum_object,
+                          storage_location: museum1.storages.first.storage_locations.first,
+                          remarks: 'ttt',
+                          inv_number: 'T.000',
+                          inv_extension: '1'}
+  let(:museum2) { create :museum_with_storage_locations }
+  let!(:object2) { create :museum_object,
+                          storage_location: museum2.storages.first.storage_locations.first,
+                          remarks: 'ttt',
+                          inv_number: 'T.000',
+                          inv_extension: '2'}
+  context 'user has extended rights' do
+    let(:user) { create :ext_user }
+
+    scenario 'fulltext search' do
+      sign_in user
+      visit '/museum_objects/search'
+      click_link 'fulltext'
+      fill_in 'fulltext_search', with: 'ttt'
+      click_on 'search'
+      expect(page).to have_text('2 results')
+    end
+
+    scenario 'inv. number search with multiple results' do
+      sign_in user
+      visit '/museum_objects/search'
+      fill_in 'inv_number_search', with: 'T.000'
+      click_on 'search'
+      expect(page).to have_text('T.000-1').and have_text('T.000-2')
+    end
+  end
+
+  context 'user has not extended rights' do
+    let(:user) { create :user, museum: museum1 }
+
+    scenario 'fulltext search' do
+      sign_in user
+      visit '/museum_objects/search'
+      click_link 'fulltext'
+      fill_in 'fulltext_search', with: 'ttt'
+      click_on 'search'
+      expect(page).to have_text('1 result')
+    end
+
+    scenario 'inv. number search with single result' do
+      sign_in user
+      visit '/museum_objects/search'
+      fill_in 'inv_number_search', with: 'T.000'
+      click_on 'search'
+      expect(page).not_to have_text('T.000-2')
+      expect(page).to have_text('T.000-1')
+    end
+  end
+end
