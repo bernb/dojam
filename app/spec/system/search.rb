fbdab739b35b88a84735f3d895b96c435cfc304d
Bernard Beitz <Bernard.Beitz@mailbox.org>
2022-04-08 16:56:47 +0200

    Add system tests for form search

--- app/spec/system/search.rb
+++ app/spec/system/search.rb
@@ -1,18 +1,21 @@
 require 'rails_helper'
 
-feature "Search for museum objects", type: :system, js: true do
+feature 'Search for museum objects', type: :system, js: true do
   let(:museum1) { create :museum_with_storage_locations }
+  let(:authenticities) {create_list(:authenticity, 5)}
   let!(:object1) { create :museum_object,
                           storage_location: museum1.storages.first.storage_locations.first,
                           remarks: 'ttt',
                           inv_number: 'T.000',
-                          inv_extension: '1'}
+                          inv_extension: '1',
+                          authenticity: authenticities.first}
   let(:museum2) { create :museum_with_storage_locations }
   let!(:object2) { create :museum_object,
                           storage_location: museum2.storages.first.storage_locations.first,
                           remarks: 'ttt',
                           inv_number: 'T.000',
-                          inv_extension: '2'}
+                          inv_extension: '2',
+                          authenticity: authenticities.first}
   context 'user has extended rights' do
     let(:user) { create :ext_user }
 
@@ -31,6 +34,17 @@ feature "Search for museum objects", type: :system, js: true do
       fill_in 'inv_number_search', with: 'T.000'
       click_on 'search'
       expect(page).to have_text('T.000-1').and have_text('T.000-2')
+    end
+
+    scenario 'form search' do
+      sign_in user
+      visit '/museum_objects/search'
+      click_link 'form'
+      select 'authenticity', from: 'selected_term'
+      click_link '+'
+      select authenticities.first.name_en, from: 'search_form_field_authenticity'
+      click_button 'search'
+      expect(page).to have_text('2 results')
     end
   end
 
@@ -53,6 +67,17 @@ feature "Search for museum objects", type: :system, js: true do
       click_on 'search'
       expect(page).not_to have_text('T.000-2')
       expect(page).to have_text('T.000-1')
+    end
+
+    scenario 'form search' do
+      sign_in user
+      visit '/museum_objects/search'
+      click_link 'form'
+      select 'authenticity', from: 'selected_term'
+      click_link '+'
+      select authenticities.first.name_en, from: 'search_form_field_authenticity'
+      click_button 'search'
+      expect(page).to have_text('1 result')
     end
   end
 end
