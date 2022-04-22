096f9032f96ad17aa4d6f698f23f94cf6250a3a7
twiegard <83067096+twiegard@users.noreply.github.com>
2022-04-22 16:54:53 +0200

    Added missing files to release script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,12 +2,26 @@
 
 set -e
 
-#export COMPOSE_FILE=git/dojam/docker/docker-compose-base.yml:git/dojam/docker/docker-compose-production.yml
 export COMPOSE_FILE=dojam/docker-compose-bare.yaml
+#export UID=$(id -u)
+#export GID=$(id -g)
 
-echo "$PWD"
+echo "Info: Running as user $(whoami) ($(id -u):$(id -g))"
+echo "Info: Working directory is $(pwd)"
 
-(docker tag dojam_app:latest dojam_app:old_latest && docker rmi dojam_app:latest) || true
-docker load -i dojam/dojam-app-latest.tar.bz2 # && docker load -o docker/dojam-db-latest.tar
+echo
+echo "Shutting down running app..."
 docker-compose down || true
+
+echo
+echo "Create db and image backup..."
+tar -czf dojam/db_backup.tar.gz dojam/db
+(docker tag dojam_app:latest dojam_app:old_latest && docker rmi dojam_app:latest) || true
+
+echo
+echo "Load new image..."
+docker load -i dojam/dojam-app-latest.tar.bz2 # && docker load -o docker/dojam-db-latest.tar
+
+echo
+echo "Restarting the app..."
 docker-compose up -d
