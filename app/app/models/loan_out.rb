aa4206fde623b1f34826e8962e0f6a10bd9aeb8e
Bernard Beitz <Bernard.Beitz@mailbox.org>
2022-04-29 15:55:59 +0200

    Add scope to LoanOut
    We use this scope for authorization as we only want normal users to show loans that belong to the same museum as the user.

--- app/app/models/loan_out.rb
+++ app/app/models/loan_out.rb
@@ -1,5 +1,13 @@
 class LoanOut < ApplicationRecord
   belongs_to :museum_object
+  scope :museum, ->(given_museum) {joins(museum_object:
+                            {storage_location:
+                               {storage: :museum}})
+                      .where(museum_object:
+                               {storage_locations:
+                                  {storages:
+                                     {museum: given_museum}}})}
+
   def overdue?
     if return_date.blank? && planned_return < DateTime.now.to_date
       return true
