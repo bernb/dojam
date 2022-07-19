e7e8925614506ad5ef7c0b394fd23b4d9b25ae93
Bernard Beitz <Bernard.Beitz@mailbox.org>
2022-07-19 10:43:08 +0200

    Fix Bug: Avoid duplicate key error, only create needed associations

--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -1,3 +1,4 @@
+# Create dummy entries as the existence of at least one entry is necessary for the application to function as intended
 storage_location = StorageLocation.create_or_find_by name_en: "undefined"
 storage = Storage.create_or_find_by name_en: "undefined"
 museum = Museum.create_or_find_by name: "undefined"
@@ -5,32 +6,37 @@ storage.storage_locations << storage_location
 museum.storages << storage
 
 
-term_categories = ["AcquisitionDeliveredBy",
-                   "AcquisitionKind",
-                   "Authenticity",
-                   "Color",
-                   "DatingCentury",
-                   "DatingMillennium",
-                   "DatingPeriod",
-                   "Decoration",
-                   "DecorationColor",
-                   "DecorationTechnique",
-                   "ExcavationSiteCategory",
-                   "ExcavationSiteKind",
-                   "InscriptionLanguage",
-                   "InscriptionLetter",
-                   "KindOfObject",
-                   "KindOfObjectSpecified",
-                   "Material",
-                   "MaterialSpecified",
-                   "PreservationMaterial",
-                   "PreservationObject",
-                   "Priority",
-                   "ProductionTechnique"]
+term_categories = %w[
+AcquisitionDeliveredBy
+AcquisitionKind
+Authenticity Color
+DatingCentury
+DatingMillennium
+DatingPeriod
+Decoration
+DecorationColor
+DecorationTechnique
+ExcavationSiteCategory
+ExcavationSiteKind
+InscriptionLanguage
+InscriptionLetter
+PreservationMaterial
+PreservationObject
+Priority
+ProductionTechnique]
 
+base_categories = %w[
+Material
+MaterialSpecified
+KindOfObject
+KindOfObjectSpecified
+]
 
-term_categories.each do |term_category|
-  term_category.constantize.create_or_find_by name_en: "undetermined"
+# Create 'undetermined' terms that work as default values.
+# A base categories' path is only to itself, ever. All other categories can have multiple paths and get seeded their
+# default path here, the 'undetermined path' with named_path == '/undetermined/undetermined/undetermined/undetermined'.
+(term_categories + base_categories).each do |category|
+  category.constantize.create_or_find_by name_en: "undetermined"
 end
 
 m = Material.first
@@ -38,7 +44,7 @@ ms = MaterialSpecified.first
 koo = KindOfObject.first
 koos = KindOfObjectSpecified.first
 
-# ToDo: Material overrides paths method for some reason. Change that back as such is highly unexpecting and bad. There also exists an after_create callback in Termlist model.
+# There exists an after_create callback in Termlist model for materials.
 #m.paths   << Path.create_or_find_by(path: "/#{m.id}")
 ms.paths  << Path.create_or_find_by(path: "/#{m.id}/#{ms.id}")
 koo.paths << Path.create_or_find_by(path: "/#{m.id}/#{ms.id}/#{koo.id}")
