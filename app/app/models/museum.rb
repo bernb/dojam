9492972a94b6b9a299777add1b439b2769dd19b4
Bernard Beitz <Bernard.Beitz@mailbox.org>
2022-07-20 08:55:44 +0200

    Fix Bug: For associations of museum model, restrict with error

--- app/app/models/museum.rb
+++ app/app/models/museum.rb
@@ -1,4 +1,4 @@
 class Museum < ApplicationRecord
-  has_many :storages
-  has_many :users
+  has_many :storages, dependent: :restrict_with_error
+  has_many :users, dependent: :restrict_with_error
 end
