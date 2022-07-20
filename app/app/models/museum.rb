ee49ca45402d4bad0849ec27a5399ef2d1c502d8
Bernard Beitz <Bernard.Beitz@mailbox.org>
2022-07-20 09:01:04 +0200

    Add method: Retrieve all museum objects
    Originally build to show object count in admin backend, but the current implementation is prone to performance issues. One might want to build a single Db call instead of merging multiple nested calls if the method might be used a lot.

--- app/app/models/museum.rb
+++ app/app/models/museum.rb
@@ -1,4 +1,8 @@
 class Museum < ApplicationRecord
   has_many :storages, dependent: :restrict_with_error
   has_many :users, dependent: :restrict_with_error
+
+  def museum_objects
+    storages.map(&:museum_objects).flatten.uniq
+  end
 end
