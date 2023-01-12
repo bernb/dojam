d7e54564fbd11acb2581b00d8bfe66611ee50a79
Bernard Beitz <Bernard.Beitz@mailbox.org>
2023-01-12 16:22:21 +0100

    Fix Bug: Don't throw error if term has no museum objects

--- app/app/admin/termlists.rb
+++ app/app/admin/termlists.rb
@@ -47,11 +47,13 @@ ActiveAdmin.register Termlist do
       row :name_en
       row :name_ar
       row :museum_objects do |m|
+        @m_objects = nil
+        next if m.museum_objects.nil?
         @m_objects = Kaminari.paginate_array(m.museum_objects).page(params[:page]).per(50)
         result = @m_objects.map{|m| link_to m.decorate.full_inv_number, admin_museum_object_path(m)}
       end
       row :page do |p|
-        [page_entries_info(@m_objects), paginate(@m_objects)]
+        [page_entries_info(@m_objects), paginate(@m_objects)] unless @m_objects.nil?
       end
       row :created_at
       row :updated_at
