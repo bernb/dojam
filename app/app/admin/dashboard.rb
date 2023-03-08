729300be12bf5c26ddf5147aafb875ca043e3364
Bernard Beitz <15048930+bernb@users.noreply.github.com>
2023-03-08 11:28:38 +0100

    Add warning to admin backend dashboard
    As the admin backend is not as fleshed out as the rest and the application and is not built with ease of use in mind, things could go wrong when using the admin backend carelessly.

--- app/app/admin/dashboard.rb
+++ app/app/admin/dashboard.rb
@@ -4,7 +4,8 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc { I18n.t("active_admin.dashboard").downcase } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
-        # span I18n.t("active_admin.dashboard_welcome.welcome")
+        span "This is the administrative backend.
+              You should only make changes here if you know what you are doing. Careless use can lead to data loss and errors!"
         # small I18n.t("active_admin.dashboard_welcome.call_to_action")
       end
     end
